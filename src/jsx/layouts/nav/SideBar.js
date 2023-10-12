8862c9ef5d6964e0a47bb33cf8cd0ac3e219a231
priya-innoura <priya@innoura.com>
2023-10-12 15:57:29 +0530

    table-changes

--- src/jsx/layouts/nav/SideBar.js
+++ src/jsx/layouts/nav/SideBar.js
@@ -1,25 +1,22 @@
 /// Menu
-import Collapse from 'react-bootstrap/Collapse';
-import React, { useState,useEffect } from 'react';
+import Collapse from "react-bootstrap/Collapse";
+import React, { useState, useEffect } from "react";
 
 /// Link
-import Link from 'next/link';
-import {useRouter} from 'next/router'; 
+import Link from "next/link";
+import { useRouter } from "next/router";
 import { SVGICON } from "../../constant/theme";
 
-
-
-import {MenuList,PhysicanMenuList} from './Menu';
-import { useDispatch , useSelector } from 'react-redux';
+import { MenuList, PhysicanMenuList } from "./Menu";
+import { useDispatch, useSelector } from "react-redux";
 import { navtoggle } from "../../../store/actions/AuthActions";
 
-
 export default function SideBar() {
-  const router = useRouter() 
-  const [stateActive, setStateActive] = useState(router.pathname);	
+  const router = useRouter();
+  const [stateActive, setStateActive] = useState(router.pathname);
   // const [sideMenu, setsideMenu] = useState(false);
-  const [userRole, setUserRole] = useState('');
-  
+  const [userRole, setUserRole] = useState("");
+
   // const openMenuToggle = () => {
   //   if(sideMenu == true){
   //     setsideMenu(false);
@@ -29,86 +26,71 @@ export default function SideBar() {
 
   //   }
 
-
   // };
 
-
   const dispatch = useDispatch();
-  const sideMenu = useSelector(state => state.sideMenu);
+  const sideMenu = useSelector((state) => state.sideMenu);
   const handleToogle = () => {
     dispatch(navtoggle());
   };
 
   useEffect(() => {
     // Perform localStorage action
-    const item = localStorage.getItem('userRole');
+    const item = localStorage.getItem("userRole");
     setUserRole(item);
-  }, [])
+  }, []);
 
- 	
   return (
-   
-    <div className='deznav show'>
-       <div className="deznav-scroll"> 
-       <div
-        className="nav-control"
-        onClick={() => {          
-          
-          handleToogle()
-        }}
-       
-      >
-        <div className={`hamburger ${sideMenu ? "is-active" : ""}`}
-          
+    <div className="deznav show">
+      <div className="deznav-scroll">
+        <div
+          className="nav-control"
+          onClick={() => {
+            handleToogle();
+          }}
         >
-          <span className="line">
-						{SVGICON.NavHeaderIcon}
-					</span>        
+          <div className={`hamburger ${sideMenu ? "is-active" : ""}`}>
+            <span className="line">{SVGICON.NavHeaderIcon}</span>
+          </div>
         </div>
+        {userRole === "admin" ? (
+          <ul className="metismenu" id="menu">
+            {MenuList.map((data, index) => {
+              return (
+                <li
+                  className={` ${stateActive === data.to ? "mm-active" : ""}`}
+                  key={index}
+                >
+                  <Link href={data.to}>
+                    <div className="menu-icon">{data.iconStyle}</div>{" "}
+                    <span className={`nav-text ${sideMenu ? "d-none" : ""}`}>
+                      {data.title}
+                    </span>
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        ) : (
+          <ul className="metismenu" id="menu">
+            {PhysicanMenuList.map((data, index) => {
+              return (
+                <li
+                  className={` ${stateActive === data.to ? "mm-active" : ""}`}
+                  key={index}
+                >
+                  <Link href={data.to}>
+                    <div className="menu-icon">{data.iconStyle}</div>{" "}
+                    <span className={`nav-text ${sideMenu ? "d-none" : ""}`}>
+                      {data.title}
+                    </span>
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        )}
       </div>
-      {userRole === "admin" ?  
-      <ul className="metismenu" id="menu">              
-              {MenuList.map((data, index)=>{               
-                    return(				
-                      <li className={` ${ stateActive === data.to ? 'mm-active' : ''}`}
-                        key={index} 
-                      >
-                          <Link  href={data.to}>
-                            <div className="menu-icon">
-                                {data.iconStyle}
-                            </div>
-                              {" "}<span  className={`nav-text ${sideMenu ? "d-none" : ""}`}>{data.title}</span>                             
-                          </Link>                   
-                       
-                      </li>	
-                    )
-                
-              })}          
-          </ul>:
-           <ul className="metismenu" id="menu">              
-           {PhysicanMenuList.map((data, index)=>{               
-                 return(				
-                   <li className={` ${ stateActive === data.to ? 'mm-active' : ''}`}
-                     key={index} 
-                   >
-                       <Link  href={data.to}>
-                         <div className="menu-icon">
-                             {data.iconStyle}
-                         </div>
-                           {" "}<span  className={`nav-text ${sideMenu ? "d-none" : ""}`}>{data.title}</span>                             
-                       </Link>                   
-                    
-                   </li>	
-                 )
-             
-           })}          
-       </ul>
-          }
-   
-
-  </div>
     </div>
-       
   );
-};
-
+}
