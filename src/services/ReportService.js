a867f19e177340b013f955f448374e4fe2947d85
Ambika-Innoura <ambika@innoura.com>
2023-12-16 12:59:34 +0530

    clear daata function added

--- src/services/ReportService.js
+++ src/services/ReportService.js
@@ -24,7 +24,7 @@ export const patientDetails = async (pagenum,startDate,endDate,search,filter) =>
 export const SentReport = async (pagenum,startDate,endDate,search) => {
   const token = localStorage.getItem("token");
   const url= search?`dbservice/reportdetails/sent?pageNo=${pagenum}&size=15&searchstring=${search}`:
-  (startDate && endDate) ?`dbservice/reportdetails/sent?pageNo=${pagenum}&size=15&startdate=${startDate}&enddate=${endDate}`:`dbservice/reportdetails/sent?pageNo=${pagenum}&size=15`
+  (startDate && endDate) ?`dbservice/reportdetails/sent?pageNo=${pagenum}&size=15&senddate=${startDate}&receiveddate=${endDate}`:`dbservice/reportdetails/sent?pageNo=${pagenum}&size=15`
   try {
     const response = await axios.get(
       `${ENDPOINTS?.apiEndoint}${url}`,
