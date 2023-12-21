8883f46e5eb9879cc93bca55b955adb28b489a02
Ambika-Innoura <ambika@innoura.com>
2023-12-21 11:40:30 +0530

    conditions added

--- src/components/table/receivedReport/ExcelDisplay.js
+++ src/components/table/receivedReport/ExcelDisplay.js
@@ -5,28 +5,33 @@ import Spinner from "../../spinner/spinner";
 import Spreadsheet, { CellBase, Matrix } from "react-spreadsheet";
 
 const ExcelDisplay = ({ tableData }) => {
-  const[data,setData]=useState()
-  const fieldNames = tableData[0];
-  const transformedData = tableData?.slice(1)?.map((row) => {
-    const rowData = {};
-    fieldNames.forEach((fieldName, index) => {
-      rowData[fieldName] = row[index];
-    });
-    return rowData;
-  });
+  const [data, setData] = useState();
+  useEffect(() => {
+    if (Array.isArray(tableData) && tableData.length > 0) {
+      const fieldNames = tableData[0];
+      const transformedData = tableData.slice(1).map((row) => {
+        const rowData = {};
+        fieldNames.forEach((fieldName, index) => {
+          rowData[fieldName] = row[index];
+        });
+        return rowData;
+      });
 
-  const formattedData = transformedData?.map((row) => {
-    const values = Object.values(row);
-    return values?.map((value) => ({ value }));
-  });
-  useEffect(()=>{
-    setData(formattedData)
-  },[tableData])
+      const formattedData = transformedData.map((row) => {
+        const values = Object.values(row);
+        return values.map((value) => ({ value }));
+      });
+
+      setData(formattedData);
+    } else {
+      setData([]);
+    }
+  }, [tableData]);
 
   return (
     <div style={{ width: "100%", height: "100%" }}>
       {tableData?.length > 0 ? (
-        <Spreadsheet data={data} onChange={setData} />
+        data && <Spreadsheet data={data} onChange={setData} />
       ) : (
         <div
           style={{
@@ -35,7 +40,7 @@ const ExcelDisplay = ({ tableData }) => {
             alignItems: "center",
           }}
         >
-          loading....
+          {data?.length === 0 ? "No data Found" : "loading...."}
         </div>
       )}
     </div>
