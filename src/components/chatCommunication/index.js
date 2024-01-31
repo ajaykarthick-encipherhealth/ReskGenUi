b6a322bc925c31998a0f9a3e24fdbb8cccaacbb9
Ajith <ajith@innoura.com>
2024-01-31 12:43:00 +0530

    chat api url changed

--- src/components/chatCommunication/index.js
+++ src/components/chatCommunication/index.js
@@ -97,7 +97,7 @@ const ChatCommunication = ({ openMsg, offMsg }) => {
   const fetchChatHistory = async (username) => {
     try {
       fetch(
-        "https://hcc.encipherhealth.com/chatservice/chatservice/api/get/history?receiver=" +
+        "https://cogent.encipherhealth.com/chatservice/api/get/history?receiver=" +
           username
       )
         .then((response) => response.json())
@@ -115,7 +115,7 @@ const ChatCommunication = ({ openMsg, offMsg }) => {
   const fetchUsers = async () => {
     try {
       const response = await fetch(
-        "https://hcc.encipherhealth.com/chatservice/chatservice/api/users"
+        "https://cogent.encipherhealth.com/chatservice/api/users"
       );
       const data = await response.json();
 
@@ -130,7 +130,7 @@ const ChatCommunication = ({ openMsg, offMsg }) => {
     }
   };
   const connect = () => {
-    let Sock = new SockJS("https://hcc.encipherhealth.com/chatservice/chatservice/ws");
+    let Sock = new SockJS("https://cogent.encipherhealth.com/chatservice/ws");
     stompClient = over(Sock);
     stompClient.connect({}, onConnected, onError);
   };
@@ -425,7 +425,7 @@ const ChatCommunication = ({ openMsg, offMsg }) => {
     try {
       // Fetch messages for the user
       const response = await fetch(
-        `https://hcc.encipherhealth.com/chatservice/chatservice/api/messages/private?sender=${sender?.secondaryUser}&receiver=${userData.username}&pageNo=${pageNumber}&pageSize=${pageSize}`
+        `https://cogent.encipherhealth.com/chatservice/api/messages/private?sender=${sender?.secondaryUser}&receiver=${userData.username}&pageNo=${pageNumber}&pageSize=${pageSize}`
       );
       const data = await response.json();
 
@@ -456,7 +456,7 @@ const ChatCommunication = ({ openMsg, offMsg }) => {
       }
     });
     if(messageIds.length > 0) {
-      await fetch("https://hcc.encipherhealth.com/chatservice/chatservice/api/change/status", {
+      await fetch("https://cogent.encipherhealth.com/chatservice/api/change/status", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -508,7 +508,7 @@ const ChatCommunication = ({ openMsg, offMsg }) => {
       try {
         // Perform the necessary API call to add the user to the database
         const response = await fetch(
-          "https://hcc.encipherhealth.com/chatservice/chatservice/api/uploadFile",
+          "https://cogent.encipherhealth.com/chatservice/api/uploadFile",
           {
             method: "POST",
 
@@ -547,7 +547,7 @@ const ChatCommunication = ({ openMsg, offMsg }) => {
       // Perform the necessary API call to add the user to the database
 
       const response = await fetch(
-        "https://hcc.encipherhealth.com/chatservice/chatservice/api/addUser",
+        "https://cogent.encipherhealth.com/chatservice/api/addUser",
         {
           method: "POST",
           headers: {
