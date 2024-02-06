57aa06471f53259a26267740bbdc0108e1af09a4
Ajith-Innoura <145423565+Ajith-Innoura@users.noreply.github.com>
2024-02-06 14:46:54 +0530

    Revert "formate dates"

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,9 @@
-import "bootstrap/dist/css/bootstrap.css";
-import "../styles/globals.css"; // Import your global CSS here
+import 'bootstrap/dist/css/bootstrap.css';
+import '../styles/globals.css'; // Import your global CSS here
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import { wrapper, store } from "../store/store";
 import { Provider } from "react-redux";
-import { PrimeReactProvider } from "primereact/api";
+import { PrimeReactProvider } from 'primereact/api';
 // import "primereact/resources/themes/lara-light-indigo/theme.css";
 //theme
 import "primereact/resources/themes/lara-light-indigo/theme.css";
@@ -12,48 +12,20 @@ import "primereact/resources/primereact.min.css";
 //icons
 
 import { config } from "@fortawesome/fontawesome-svg-core";
-import Footer from "../jsx/layouts/Footer";
-import { useEffect } from "react";
-import dynamic from "next/dynamic";
-import { getChatReply } from "../store/actions/DashboardActions";
-import { useSelector } from "react-redux";
+import Footer from '../jsx/layouts/Footer';
 config.autoAddCss = false;
 
 function MyApp({ Component, pageProps }) {
-  const msgReply = useSelector((state) => state.workFlow.chatReply);
-
-  const handleNewUserMessage = (newMessage) => {
-    dispatch(getChatReply(newMessage));
-  };
-  const handleQuickButtonClicked = (data) => {
-    console.log(data);
-  };
-  const TerminalComponent = dynamic(
-    () => import("react-chat-widget").then((mod) => mod.Widget),
-    {
-      ssr: false,
-    }
-  );
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      const { addResponseMessage } = require("react-chat-widget");
-      addResponseMessage(msgReply ? msgReply : "Welcome to CogentAI!");
-    }
-  }, []);
   return (
     <>
       <PrimeReactProvider>
-        <Provider store={store}>
-          <TerminalComponent
-            handleNewUserMessage={handleNewUserMessage}
-            handleQuickButtonClicked={handleQuickButtonClicked}
-            showBadge={false}
-            emojis={true}
-            title="CogentAI"
-            subtitle="Chat with CogentAI"
-          />
-          <Component {...pageProps} />
-        </Provider>
+      <Provider store={store}>
+        
+        
+        <Component {...pageProps} />
+      
+
+      </Provider>
       </PrimeReactProvider>
     </>
   );
