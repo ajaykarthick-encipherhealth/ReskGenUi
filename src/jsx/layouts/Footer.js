282c5de8fc58c38d2f3f591ceecf80ce1aeb1e87
priya-innoura <priya@innoura.com>
2024-02-06 12:21:03 +0530

    new-tab-issue-fix

--- src/jsx/layouts/Footer.js
+++ src/jsx/layouts/Footer.js
@@ -1,101 +1,3 @@
-// import React from "react";
-
-// const Footer = () => {
-// 	var d = new Date();
-// 	return (
-// 		<div >
-// 			<div className="footer out-footer" style={{display:"flex", alignItems:"center", justifyContent:"center", backgroundColor:"#f0f6fe"}}>
-// 			<div className="copyright" style={{display:"flex", alignItems:"center"}}>
-// 				<p>Copyright ©
-// 					{" "}Developed by{" "}
-// 					<a href="http://innoura.com/" target="_blank"  rel="noreferrer">
-// 					Encipherhealth
-// 					</a>{" "}
-// 					{d.getFullYear()}
-// 				<ul style={{display:"flex", alignItems:"center", justifyContent:"space-between"}}>
-//                        <li>
-//                            <a href="#">Terms</a>
-//                        </li>
-//                        <li>
-//                            <a href="#">Privacy</a>
-//                        </li>
-//                        <li>
-//                            <a href="#">Security</a>
-//                        </li>
-//                        <li>
-//                            <a href="#">Status</a>
-//                        </li>
-//                        <li>
-//                            <a href="#">Docs</a>
-//                        </li>
-//                        <li>
-//                            <a href="#">Contact</a>
-//                        </li>
-//                    </ul>
-// 				</p>
-
-// 			</div>
-
-// 		</div>
-// 		</div>
-
-// 	);
-// };
-
-// export default Footer;
-// // import React from "react";
-
-// // const Footer = () => {
-// //     const currentYear = new Date().getFullYear();
-
-// //     return (
-// //         <div className="footer">
-// //             <div className="footer-content">
-// //                 <div className="footer-section">
-// //                     <h3>Footer navigation</h3>
-// //                     <ul>
-// //                         <li>
-// //                             <a href="#">Terms</a>
-// //                         </li>
-// //                         <li>
-// //                             <a href="#">Privacy</a>
-// //                         </li>
-// //                         <li>
-// //                             <a href="#">Security</a>
-// //                         </li>
-// //                         <li>
-// //                             <a href="#">Status</a>
-// //                         </li>
-// //                         <li>
-// //                             <a href="#">Docs</a>
-// //                         </li>
-// //                         <li>
-// //                             <a href="#">Contact</a>
-// //                         </li>
-// //                     </ul>
-// //                 </div>
-
-// //                 <div className="footer-section">
-// //                     <h3>Legal</h3>
-// //                     <ul>
-// //                         <li>
-// //                             <a href="#">Manage cookies</a>
-// //                         </li>
-// //                         <li>
-// //                             <a href="#">Do not share my personal information</a>
-// //                         </li>
-// //                     </ul>
-// //                 </div>
-// //             </div>
-
-// //             <div className="footer-bottom">
-// //                 <p>© {currentYear} GitHub, Inc.</p>
-// //             </div>
-// //         </div>
-// //     );
-// // };
-
-// // export default Footer;
 
 import React from "react";
 import { IMAGES, SVGICON } from "../constant/theme";
@@ -105,8 +7,7 @@ import Image from "next/image";
 const Footer = () => {
  const currentYear = new Date().getFullYear();
  const handleEncipherhealthClick = () => {
-   // You can use window.location.href or any router navigation method
-   window.location.href = "https://encipherhealth.com/";
+   window.open("https://encipherhealth.com/", "_blank");
  };
  return (
    <footer className="text-center">
