185ca95a16973c7b8f81513f267af352b57cfda5
Ambika-Innoura <ambika@innoura.com>
2024-02-08 14:16:32 +0530

    validate year

--- src/components/headerFilters/functions.js
+++ src/components/headerFilters/functions.js
@@ -451,3 +451,31 @@ export const validateConfirmPassword = (
 
   return true;
 };
+
+export const validateYear = (year, setErrors) => {
+  const yearPattern = /^[0-9]{4}$/;
+  const correctYear = parseInt(year) > 0;
+  const currentYear = new Date().getFullYear();
+
+  if (year?.length === 0) {
+    setErrors({
+      year: "Please enter year",
+    });
+
+    return false;
+  }
+  if (!yearPattern.test(year) && !correctYear) {
+    setErrors({
+      year: "Please enter a valid 4-digit positive year",
+    });
+    return false;
+  }
+  if (year > currentYear || year?.length < 4) {
+    setErrors({
+      year: "Please enter a valid year",
+    });
+    return false;
+  }
+
+  return true;
+};
