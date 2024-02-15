149c77820cc173e4617de15f2d2255ad3f4de63d
Ambika-Innoura <ambika@innoura.com>
2024-02-15 19:00:14 +0530

    add styles

--- src/components/table/admin/FileProcessing/FileProcessing.js
+++ src/components/table/admin/FileProcessing/FileProcessing.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import TableStyle from "../../table.module.css";
-import { Empty, Progress, Steps, Tooltip } from "antd";
+import { Empty, Progress,Steps, Tooltip } from "antd";
 import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
 import {
@@ -10,7 +10,7 @@ import {
 import ENDPOINTS from "../../../../utility/enpoints";
 import { DownOutlined, UpOutlined } from "@ant-design/icons";
 import SpinnerDots from "../../../spinner";
-import dayjs from 'dayjs';
+import dayjs from "dayjs";
 
 export const eventStreming = (
   ENDPOINTS,
@@ -472,7 +472,7 @@ function FileProcessingTable({ patinetListAll }) {
               <div className={TableStyle.fileprocessing}>
                 {mappedSteps?.length > 0 &&
                   mappedSteps?.map((step, index) => {
-                    const findData = selectedRowTime?.find(
+                    const findData = selectedRowTime?.data?.find(
                       (item) => item?.processStageChart === step?.info
                     );
                     return (
@@ -480,14 +480,20 @@ function FileProcessingTable({ patinetListAll }) {
                         key={index}
                         className={TableStyle.innerProcessingDiv}
                       >
-                        {selectedRowTime?.length > 0 && findData ? (
-                          <span>
-                            {findData?.createdDate
-                              ? dayjs(findData?.createdDate).format("hh:mm:ss A")
-                              : "---"}
-                          </span>
+                        {selectedRowTime?.data?.length > 0 ? (
+                          findData ? (
+                            <span>
+                              {findData?.createdDate
+                                ? dayjs(findData?.createdDate).format(
+                                    "hh:mm:ss A"
+                                  )
+                                : "---"}
+                            </span>
+                          ) : (
+                            "---"
+                          )
                         ) : (
-                          "---"
+                          "Loading..."
                         )}
                       </div>
                     );
