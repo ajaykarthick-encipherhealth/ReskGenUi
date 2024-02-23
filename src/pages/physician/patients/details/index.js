83f0d24b4ebccf2ab7ff32c5056deab652bc77dc
priya-innoura <priya@innoura.com>
2024-02-23 19:07:50 +0530

    alignment issue fix

--- src/pages/physician/patients/details/index.js
+++ src/pages/physician/patients/details/index.js
@@ -1656,7 +1656,7 @@ const Details = ({}) => {
                     <div className="row">
                       <div
                         className="col-xl-1 col-sm-12"
-                        style={{ zIndex: "999" }}
+                        style={{ zIndex: "1" }}
                       >
                         <Button
                           onClick={backToPatientData}
