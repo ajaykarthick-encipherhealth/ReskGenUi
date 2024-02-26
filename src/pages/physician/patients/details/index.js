46868054d54366b72fa2c34395c0cde516f7ad05
Ajith <ajith@innoura.com>
2024-02-26 17:33:21 +0530

    issue fixed

--- src/pages/physician/patients/details/index.js
+++ src/pages/physician/patients/details/index.js
@@ -1953,7 +1953,7 @@ const Details = ({}) => {
                               ALLOCATE
                             </Dropdown.Button>
                           </div>
-                        ) : userRole == "SUPERVISOR" ? (
+                        ) : userRole == "supervisor" ? (
                           <div className={`${visitStyles.actionbtnContainer}`}>
                             <Dropdown.Button
                               type="primary"
