22cfb073e9a966727057ba75386facbcc041687f
priya-innoura <priya@innoura.com>
2024-02-28 12:02:08 +0530

    removed local

--- src/utility/enpoints.js
+++ src/utility/enpoints.js
@@ -1,5 +1,5 @@
 const ENDPOINTS = {
-  apiEndoint: "https://local.hcc.encipherhealth.com/secure/",
+  apiEndoint: "https://hcc.encipherhealth.com/secure/",
   apiEndointFileUpload: "http://13.68.177.51:8082/ai/",
   apiEndointFileUploadHcc: "https://hcc.encipherhealth.com/secure/",
 };
