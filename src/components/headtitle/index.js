eb580304348cf176913f758006e90e4088271a0f
Ambika-Innoura <ambika@innoura.com>
2024-03-05 16:14:31 +0530

    rename

--- src/components/headtitle/index.js
+++ src/components/headtitle/index.js
@@ -133,7 +133,7 @@ const HeadTitle = ({
               setSelectedDates([]);
             }}
           >
-            <Button disabled={isDisabled ? true : false}>Clear</Button>
+            <Button disabled={isDisabled ? true : false}>Clear Date</Button>
           </div>
         </div>
         <div id="date-popup" style={{ position: "relative" }} />
