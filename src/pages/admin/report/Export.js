5f218ff6378a122b9268cf061d50f5eae1c8f8a8
Ambika-Innoura <ambika@innoura.com>
2024-03-05 17:52:27 +0530

    chnaged getrole api for admin

--- src/pages/admin/report/Export.js
+++ src/pages/admin/report/Export.js
@@ -2,9 +2,8 @@ import { Button, Checkbox, Form, Input, Modal, Radio, Select } from "antd";
 import React, { useEffect, useState } from "react";
 import styles from "./report.module.css";
 import {
-  getExportDetails,
-  getUsersList,
-} from "../../../store/actions/ReportActions";
+  getExportDetails, getUsersList,
+} from "../../../store/actions/adminAction/ReportActions";
 import { useDispatch, useSelector } from "react-redux";
 const { Option } = Select;
 
