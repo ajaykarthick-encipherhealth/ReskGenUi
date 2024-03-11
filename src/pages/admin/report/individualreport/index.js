c247d1da9959f8269ea26604dda6046f461f61c3
Ambika-Innoura <ambika@innoura.com>
2024-03-11 11:56:49 +0530

    chngesdone

--- src/pages/admin/report/individualreport/index.js
+++ src/pages/admin/report/individualreport/index.js
@@ -280,7 +280,7 @@ const IndividualReceiverReport = () => {
             <div>
               {" "}
               <Image src={send} alt="noimg" />
-              &nbsp;Sender:&nbsp;
+              &nbsp;{isSentReport?"Reciever":"Sender"}:&nbsp;
               {reportInfo?.sender}
             </div>
             <div>
