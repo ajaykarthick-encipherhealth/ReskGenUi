3f836fbc75b2d8f2da4070966980fbceaa09dd4e
Ambika-Innoura <ambika@innoura.com>
2024-03-11 16:16:20 +0530

    code validated

--- src/pages/twofactorAuthentication/Authentication.js
+++ src/pages/twofactorAuthentication/Authentication.js
@@ -18,7 +18,7 @@ export const generateCodeArray = () =>
 const index = () => {
   const router = useRouter();
   const dispatch = useDispatch();
-  const [seconds, setSeconds] = useState(30);
+  const [seconds, setSeconds] = useState(10);
   const [enableMFA, setEnableMFA] = useState(false);
   const [username, setUsername] = useState();
   const [skip, setSkip] = useState();
@@ -47,22 +47,39 @@ const index = () => {
     setPassword(decodedParams?.password);
     const skipParam = decodedParams?.skipEntry;
     setSkip(skipParam);
-    const intervalId = setInterval(() => {
-      setSeconds((prevSeconds) => {
-        if (prevSeconds === 0) {
-          clearInterval(intervalId);
-        }
-        return Math.max(0, prevSeconds - 1);
-      });
-    }, 1000);
+    // const intervalId = setInterval(() => {
+    //   setSeconds((prevSeconds) => {
+    //     if (prevSeconds === 0) {
+    //       clearInterval(intervalId);
+    //     }
+    //     return Math.max(0, prevSeconds - 1);
+    //   });
+    // }, 1000);
 
-    return () => clearInterval(intervalId);
+    // return () => clearInterval(intervalId);
   }, []);
   useEffect(() => {
     if (seconds === 0) {
-      setInputValues(Array.from({ length: generateCodeArray().length }, () => ''));
+      setCode([]);
+      inputRefs[1].current.focus();
+      setSeconds(30);
     }
   }, [seconds]);
+
+  useEffect(() => {
+    if (code?.length > 0) {
+      const intervalId = setInterval(() => {
+        setSeconds((prevSeconds) => {
+          if (prevSeconds === 0) {
+            clearInterval(intervalId);
+          }
+          return Math.max(0, prevSeconds - 1);
+        });
+      }, 1000);
+      return () => clearInterval(intervalId);
+    }
+  }, [code]);
+
   return (
     <div className={styles.maindiv}>
       <section className={styles.innerdiv}>
@@ -85,6 +102,7 @@ const index = () => {
                     type="text"
                     maxLength="1"
                     pattern="[0-9]"
+                    value={code?.length > 0 ? code[index] : ""}
                     className={styles.codeInput}
                     onInput={(e) => handleInput(index, e)}
                     ref={inputRefs[index]}
