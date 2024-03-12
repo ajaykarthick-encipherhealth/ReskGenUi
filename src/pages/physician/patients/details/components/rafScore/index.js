e47f60d2cd8abd90018c1ee1668d0309622fcc0f
Ajith <ajith@innoura.com>
2024-03-12 11:15:41 +0530

    raf score issue fixed

--- src/pages/physician/patients/details/components/rafScore/index.js
+++ src/pages/physician/patients/details/components/rafScore/index.js
@@ -169,7 +169,7 @@ const RafScore = ({ rafScoreList }) => {
                   <div className={style.titleHead}>
                     <div className="row">
                       <div className="col-xl-6">V24 score</div>
-                      <div className="col-xl-6">V24Score(33%)</div>
+                      <div className="col-xl-6">V24Score(67%)</div>
                     </div>
                   </div>
                   <div className={style.detailsHead}>
@@ -186,7 +186,7 @@ const RafScore = ({ rafScoreList }) => {
                   <div className={style.titleHead}>
                     <div className="row">
                       <div className="col-xl-6">V28 score</div>
-                      <div className="col-xl-6">V28Score(67%)</div>
+                      <div className="col-xl-6">V28Score(33%)</div>
                     </div>
                   </div>
                   <div className={style.detailsHead}>
