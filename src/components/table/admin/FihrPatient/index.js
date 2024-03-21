1218e64ef56642770b0454570aab76e4d0c8e047
vignesh-innoura <vignesh@innoura.com>
2024-03-21 11:37:16 +0530

    patient table

---
+++ src/components/table/admin/FihrPatient/index.js
@@ -0,0 +1,184 @@
+import React, { useEffect } from "react";
+import { Badge, Empty, Popover, Tooltip } from "antd";
+import TableStyle from "../../table.module.css";
+import { SVGICON } from "../../../../jsx/constant/theme";
+import { Paginator } from "primereact/paginator";
+import { selectedRow } from "../../../../store/actions/ReportActions";
+import { useDispatch } from "react-redux";
+import dayjs from "dayjs";
+import {
+  dateFormate,
+  renderUserPrfoileAvatar,
+  sortFunction,
+} from "../../../headerFilters/functions";
+import moment from "moment";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleUser } from "@fortawesome/free-solid-svg-icons";
+
+function FIHRPatinetTable({
+  reportListAll,
+  paginationFirst,
+  ReportPatientDetails,
+  onPageChange,
+  selectedRows,
+  tableData,
+}) {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(selectedRow(selectedRows));
+  }, [selectedRows]);
+
+  const dateFormateAlign = (dates) => {
+    return dates?.map((res, index) => {
+      if (index < 1) {
+        var sectionMapArr = <span>{moment(res).year()}</span>;
+        return sectionMapArr;
+      } else if (dates.length - 1 == index) {
+        var sectionMapArr = (
+          <Popover
+            content={
+              <>
+                {dates?.map((item, i) =>
+                  i > 0 ? (
+                    <div className="text-center">{moment(item).year()}</div>
+                  ) : null
+                )}
+              </>
+            }
+            placement="bottom"
+          >
+            <span
+              style={{ fontSize: "10px" }}
+              className={`border border-success-subtle mx-1 p-1 rounded-circle font`}
+            >
+              {dates.length - 1}+
+            </span>
+          </Popover>
+        );
+        return sectionMapArr;
+      }
+    });
+  };
+
+  return (
+    <div className={TableStyle.classContaineer}>
+      {reportListAll?.data?.length === 0 ? (
+        <Empty />
+      ) : (
+        <table className={TableStyle.classTable}>
+          <thead className={TableStyle.classTTotalhead}>
+            <tr>
+              <>
+                <th>BATCH ID</th>
+                <th>PATIENT COUNT</th>
+                {/* <th
+                  style={{ cursor: "pointer" }}
+                  onClick={() => {
+                    sortFunction(
+                      sortOrder,
+                      setSortOrder,
+                      setSort,
+                      "processedDate"
+                    );
+                  }}
+                >
+                  COMPLETED DATE{" "}
+                  {sortOrder === "ASC" ? (
+                    <ArrowUpOutlined />
+                  ) : (
+                    <ArrowDownOutlined />
+                  )}
+                </th> */}
+                <th>STATUS </th>
+                <th>YEAR OF SERVICE</th>
+                <th className={TableStyle.rowAudited}>INITIATED BY </th>
+                <th>BATCH INITIATED DATE </th>
+              </>
+            </tr>
+          </thead>
+
+          <tbody className={TableStyle.bodytable}>
+            {tableData.length > 0 ? (
+              tableData.map((row, index) => (
+                <tr key={index}>
+                  <>
+                    <td className={TableStyle.childBorder}>
+                      {row?.batchID ? row?.batchID : "---"}
+                    </td>
+
+                    <td className={TableStyle.childBorder}>
+                      {row?.patientCount ? row?.patientCount : "---"}
+                    </td>
+                    <td className={TableStyle.childBorder}>
+                      <div>
+                        <span className="text-capitalize mx-2">
+                          {row.status}
+                        </span>
+                        <span>{row.statusValue}</span>
+                      </div>
+                    </td>
+
+                    <td className={TableStyle.childBorder}>
+                      {row?.yearOfService
+                        ? dateFormateAlign(row?.yearOfService)
+                        : "000"}
+                    </td>
+                    <td
+                      className={TableStyle.childBorder}
+                      style={{ textAlign: "left", paddingLeft: "110px" }}
+                    >
+                      {row.initiatedByFirstName ||
+                      row.initiatedByLastName ||
+                      row.auditedByProfileImage ? (
+                        <div style={{ display: "flex", alignItems: "center" }}>
+                          {" "}
+                          <span style={{ marginRight: "10px" }}>
+                            {" "}
+                            {renderUserPrfoileAvatar(
+                              row.initiatedByFirstName,
+                              row.initiatedByLastName,
+                              row.auditedByProfileImage,
+                              "header"
+                            )}
+                          </span>
+                          <span>
+                            {row.initiatedByFirstName} {row.initiatedByLastName}
+                          </span>
+                        </div>
+                      ) : (
+                        <div style={{ paddingLeft: "70px" }}>---</div>
+                      )}
+                    </td>
+                    <td className={TableStyle.childBorder}>
+                      {dateFormate(dayjs, row?.initialedDate)}
+                    </td>
+                  </>
+                </tr>
+              ))
+            ) : (
+              <tr>
+                <td colSpan={11}>
+                  <Empty />
+                </td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      )}
+      <div className="pagination-container">
+        <Paginator
+          first={paginationFirst}
+          rows={15}
+          totalRecords={ReportPatientDetails?.totalElements}
+          onPageChange={onPageChange}
+        />
+        <div className="total-pages">
+          Total count: {ReportPatientDetails?.totalElements}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default FIHRPatinetTable;
---
+++ src/pages/admin/fihrTable/Export.js
@@ -0,0 +1,503 @@
+import { Button, Checkbox, Form, Input, Modal, Radio, Select } from "antd";
+import React, { useEffect, useState } from "react";
+import styles from "./report.module.css";
+import {
+  getExportDetails,
+  getUsersList,
+} from "../../../store/actions/adminAction/ReportActions";
+import { useDispatch, useSelector } from "react-redux";
+import { updateSentReport } from "../../../services/adminServices/ReportService";
+import { getActiveTab } from "../../../store/actions/l2Action/AuditReportAction";
+
+const { Option } = Select;
+
+export const checkBoxData = [
+  {
+    id: 1,
+    title: "patientId",
+    heading: "Patient ID",
+    checked: false,
+  },
+  {
+    id: 2,
+    title: "patientName",
+    heading: "Patient Name",
+    checked: false,
+  },
+  {
+    id: 3,
+    title: "dob",
+    heading: "Dob",
+    checked: false,
+  },
+  {
+    id: 4,
+    title: "processedDate",
+    heading: "Processed Date",
+    checked: false,
+  },
+  {
+    id: 5,
+    title: "providerName",
+    heading: "Provider Name",
+    checked: false,
+  },
+  {
+    id: 6,
+    title: "allocatedOn",
+    heading: "Allocated On",
+    checked: false,
+  },
+  {
+    id: 7,
+    title: "noOfValidCodes",
+    heading: "No Of Valid Codes",
+    checked: false,
+  },
+  {
+    id: 8,
+    title: "noOfSuggestedCodes",
+    heading: "No Of Suggested Codes",
+    checked: false,
+  },
+  {
+    id: 9,
+    title: "noOfDeletedCodes",
+    heading: "No Of Deleted Codes",
+    checked: false,
+  },
+  {
+    id: 10,
+    title: "totalCodes",
+    heading: "Total Codes",
+    checked: false,
+  },
+  {
+    id: 11,
+    title: "allocatedUserId",
+    heading: "Allocated UserId",
+    checked: false,
+  },
+  {
+    id: 12,
+    title: "comments",
+    heading: "Comments",
+    checked: false,
+  },
+  {
+    id: 13,
+    title: "validDisease",
+    heading: "Valid Disease",
+    checked: false,
+  },
+];
+
+export const debounce = (func, delay) => {
+  let timer;
+  return function (...args) {
+    const context = this;
+    clearTimeout(timer);
+    timer = setTimeout(() => func.apply(context, args), delay);
+  };
+};
+const Export = ({
+  isModalVisible,
+  closeModal,
+  rowsLength,
+  setIsModalVisible,
+  setSelectedRows,
+  setSelectAll,
+  selectedRows,
+  isSent,
+}) => {
+  const dispatch = useDispatch();
+  const usersList = useSelector((state) => state.report?.usersList);
+  const [selectedUser, setSelectedUser] = useState();
+  const [search, setSearch] = useState("");
+  const [display, setDisplay] = useState(false);
+  const [userList, setUsersList] = useState([]);
+  const [selectedList, setSelectedList] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [currentUser, setCurrentUser] = useState("");
+  const [removedUsers, setRemovedUsers] = useState([]);
+  const [reportName, setReportName] = useState("");
+  const [form] = Form.useForm();
+
+  const [checkall, setCheckAll] = useState(checkBoxData);
+
+  useEffect(() => {
+    setCurrentUser(localStorage.getItem("userId"));
+
+    var orgId = localStorage.getItem("orgId");
+    dispatch(getUsersList(orgId, search));
+  }, [search]);
+
+  const options = usersList?.response
+    ?.map(
+      (data) =>
+        data?.userName !== currentUser && {
+          label: (
+            <span>
+              {data?.firstName}&nbsp;&nbsp;{data?.lastName}
+            </span>
+          ),
+          value: data?.userName,
+        }
+    )
+    .filter(Boolean);
+
+  const handleSelectedOption = (value) => {
+    const filteredData = usersList?.response?.filter(
+      (data) => data?.userName === value[0]
+    );
+
+    setSelectedList(filteredData?.map((item) => item?.userName));
+    setSelectedUser((prevUsers) => [
+      { ...prevUsers, user: value[0], role: null },
+    ]);
+    setOpen(false);
+  };
+
+  const handleSelectedRole = (value) => {
+    setSelectedUser((prevUsers) =>
+      prevUsers?.map((item) => {
+        if (value) {
+          return { ...item, role: value };
+        }
+        return item;
+      })
+    );
+  };
+
+  const filteredOptions = options?.filter((option) => {
+    return !userList?.some((data) => option?.value === data?.user);
+  });
+
+  const debouncedSearch = debounce((value) => {
+    setSearch(value);
+  }, 300);
+  const handleSearch = (e) => {
+    debouncedSearch(e);
+  };
+  const onFinish = (values) => {
+    const patientIds = rowsLength?.map((item) => item?.patientId);
+    const editUserAndAccess = userList.reduce((result, { user, role }) => {
+      if (Array.isArray(user)) {
+        user.forEach((info) => {
+          result[info.userName] = role;
+        });
+      } else {
+        result[user] = role;
+      }
+      return result;
+    }, {});
+    const fields = checkall?.reduce((acc, data) => {
+      acc[data?.title] = data?.checked;
+      return acc;
+    }, {});
+    const data = {
+      fields: fields,
+      patientIds: patientIds,
+      fileType: values?.ReportTYpe,
+      reportName: values?.ReportName,
+      userAndAccess: editUserAndAccess,
+    };
+
+    const updatedData = {
+      reportName: values?.ReportName,
+      reportId: selectedRows?._id,
+      userAndAccess: editUserAndAccess,
+      removedUsers: removedUsers,
+    };
+
+    if (!isSent) {
+      dispatch(getExportDetails(data));
+    } else {
+      dispatch(updateSentReport(updatedData));
+      dispatch(
+        getActiveTab(
+          "SentReport"
+        )
+      );
+    }
+    form.resetFields();
+    setUsersList([]);
+    setCheckAll((prev) => {
+      return prev?.map((data) => {
+        return { ...data, checked: false };
+      });
+    });
+    setSelectedRows([]);
+    setSelectAll(false);
+    setTimeout(() => {
+      setIsModalVisible(false);
+    }, 500);
+  };
+
+  const deleteUser = (userInfo) => {
+    if (Array?.isArray(userInfo)) {
+      setUsersList((prevUserList) =>
+        prevUserList?.filter(
+          (info) => info?.user[0]?.userId !== userInfo[0]?.userId
+        )
+      );
+    } else {
+      setRemovedUsers((prev) => [...prev, userInfo]);
+      setUsersList((prevUserList) =>
+        prevUserList?.filter((info) => info?.user !== userInfo)
+      );
+    }
+  };
+  useEffect(() => {
+    setSelectedList([]);
+    if (selectedRows?.receivedUsers?.length > 0) {
+      setDisplay(true);
+
+      setUsersList(selectedRows?.receivedUsers);
+    } else {
+      setDisplay(false);
+      setUsersList([]);
+    }
+  }, [selectedRows, isModalVisible]);
+  form.setFieldsValue({
+    ReportName:
+      selectedRows?.receivedUsers?.length > 0
+        ? selectedRows?.reportName
+        : reportName,
+  });
+
+  return (
+    <Modal
+      title="Export "
+      visible={isModalVisible}
+      onCancel={closeModal}
+      footer={false}
+      className={styles.modelCon}
+    >
+      <Form form={form} name="basic" onFinish={onFinish}>
+        <Form.Item
+          label="Report Name"
+          name="ReportName"
+          rules={[
+            {
+              required: true,
+              message: "Please input your ReportName!",
+            },
+          ]}
+        >
+          <Input
+            disabled={selectedRows?.reportName ? true : false}
+            onChange={(e) => setReportName(e.target.value)}
+          />
+        </Form.Item>
+        {!isSent && (
+          <>
+            <Form.Item
+              label="Report Type"
+              name="ReportTYpe"
+              rules={[
+                {
+                  required: true,
+                  message: "Please Select your Option!",
+                },
+              ]}
+            >
+              <Radio.Group>
+                <Radio.Button value="EXCEL" className={styles.excel}>
+                  Excel
+                </Radio.Button>
+                <Radio.Button value="CSV" className={styles.excel}>
+                  CSV
+                </Radio.Button>
+              </Radio.Group>
+            </Form.Item>
+
+            <Form.Item
+              label="Report Fields"
+              name="ReportFields"
+              rules={[
+                {
+                  required: true,
+                  message: "Please Select your Option!",
+                },
+              ]}
+            >
+              <div
+                style={{
+                  display: "grid",
+                  gridTemplateColumns: "repeat(3, 1fr)",
+                  gap: "16px",
+                  margin: "0px 0",
+                }}
+              >
+                <Checkbox
+                  key={0}
+                  value={"all"}
+                  onChange={(e) => {
+                    if (e.target.value === "all") {
+                      setCheckAll((prev) => {
+                        return prev?.map((data) => {
+                          return { ...data, checked: e.target.checked };
+                        });
+                      });
+                    }
+                  }}
+                >
+                  {" "}
+                  Check All
+                </Checkbox>
+                {checkall?.map((data) => (
+                  <>
+                    <Checkbox
+                      key={data?.id}
+                      value={data?.title}
+                      checked={data?.checked}
+                      onChange={(e) => {
+                        setCheckAll((prev) => {
+                          return prev?.map((data) => {
+                            if (data?.title === e.target.value) {
+                              return { ...data, checked: e.target.checked };
+                            } else {
+                              return data;
+                            }
+                          });
+                        });
+                      }}
+                    >
+                      {data.heading}
+                    </Checkbox>
+                  </>
+                ))}
+              </div>
+            </Form.Item>
+          </>
+        )}
+        <div style={{ display: "flex", marginBottom: "20px" }}>
+          <div style={{ width: "100%" }}>
+            <Form.Item
+              label="Send To"
+              name="User"
+              rules={[
+                {
+                  required: false,
+                  message: "Select User",
+                },
+              ]}
+            >
+              <div
+                style={{
+                  width: "99%",
+                  display: "flex",
+                  justifyContent: "space-between",
+                }}
+              >
+                <Select
+                  mode="multiple"
+                  placeholder="Please select"
+                  onChange={handleSelectedOption}
+                  onSearch={handleSearch}
+                  className={styles.selectDiv}
+                  value={selectedList}
+                  open={open}
+                  onDropdownVisibleChange={(visible) => setOpen(visible)}
+                >
+                  {filteredOptions?.map((data) => (
+                    <Option key={data?.value} value={data?.value}>
+                      {data?.label}
+                    </Option>
+                  ))}
+                </Select>
+                <Select
+                  // mode="multiple"
+                  placeholder="Please select"
+                  onChange={(value) => handleSelectedRole(value)}
+                  className={styles.selectDiv}
+                  value={selectedUser?.map((item) => item.role)}
+                >
+                  <Option value="READ">Read</Option>
+                  <Option value="DOWNLOAD">Download</Option>
+                </Select>
+                <Button
+                  onClick={() => {
+                    setDisplay(true);
+                    setSelectedUser([]);
+                    setUsersList((prev) => [...prev, ...selectedUser]);
+                    setSelectedList([]);
+                  }}
+                  style={{
+                    backgroundColor: "#04306f",
+                    width: "100px",
+                    color: "#fff",
+                  }}
+                  disabled={
+                    selectedUser?.length > 0 &&
+                    selectedUser[0]?.user &&
+                    selectedUser[0]?.role
+                      ? // selectedUser[0]?.user?.length <= 1
+                        false
+                      : true
+                  }
+                >
+                  add
+                </Button>
+              </div>
+            </Form.Item>
+          </div>
+        </div>
+        <div className={styles.displayDiv}>
+          {display ? (
+            <div>
+              {userList?.map((item, index) => (
+                <div className={styles.userName}>
+                  <div key={index} className={styles.userRoleContainer}>
+                    {item?.user?.length > 0 && Array.isArray(item?.user)
+                      ? item?.user?.map(
+                          (info) => `${info?.firstName}  ${info?.lastName}`
+                        )
+                      : item?.user}
+                  </div>
+                  <div key={index} className={styles.userRoleContainer}>
+                    {item?.role}
+                  </div>
+
+                  <div
+                    style={{ cursor: "pointer" }}
+                    onClick={() => deleteUser(item?.user)}
+                  >
+                    X
+                  </div>
+                </div>
+              ))}
+            </div>
+          ) : (
+            "No Users Selected"
+          )}
+        </div>
+
+        <Form.Item
+          wrapperCol={{
+            offset: 8,
+            span: 16,
+          }}
+          className={styles.footerBtn}
+        >
+          <Button
+            type="primary"
+            htmlType="submit"
+            style={{
+              backgroundColor: "#04306f",
+              color: "#fff",
+              width: "100px",
+              height: "40px",
+            }}
+            disabled={userList?.length > 0 ? false : true}
+          >
+            Generate
+          </Button>
+        </Form.Item>
+      </Form>
+    </Modal>
+  );
+};
+
+export default Export;
