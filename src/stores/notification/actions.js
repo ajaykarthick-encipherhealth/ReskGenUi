3ee4f120f1197ba27d28879bc774bdcfec5d377b
Ambika-Innoura <ambika@innoura.com>
2024-03-28 16:40:39 +0530

    reframed files

--- src/stores/notification/actions.js
+++
@@ -1,7 +0,0 @@
-import { createActionThunk } from "../../utils/redux";
-import * as network from "./network";
-
-export const loadNotification = createActionThunk(
-  "NOTIFICATION",
-  network.loadNotification
-);
