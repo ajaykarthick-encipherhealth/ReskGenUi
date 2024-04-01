c79d84e41420e8834b4365ad5cb711e036a20735
vignesh-innoura <vignesh@innoura.com>
2024-04-01 17:16:40 +0530

    change apiEnpoint

--- src/utility/enpoints.js
+++ src/utility/enpoints.js
@@ -1,7 +1,7 @@
 const ENDPOINTS = {
-  apiEndoint: "https://local.hcc.encipherhealth.com/secure/",
+  apiEndoint: "https://hcc.encipherhealth.com/secure/",
   apiEndointFileUpload: "http://13.68.177.51:8082/ai/",
-  apiEndointFileUploadHcc: "https://local.hcc.encipherhealth.com/secure/",
+  apiEndointFileUploadHcc: "https://hcc.encipherhealth.com/secure/",
   apiLocal: "http://localhost:3003/",
   isShow: false
 };
