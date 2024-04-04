2436703ece8f8f5a7998233862d6469d80da1327
vignesh-innoura <vignesh@innoura.com>
2024-04-04 13:29:27 +0530

    .env comfingeration

--- src/utility/enpoints.js
+++ src/utility/enpoints.js
@@ -1,8 +1,7 @@
 const ENDPOINTS = {
-  apiEndoint: "https://local.hcc.encipherhealth.com/secure/",
-  apiEndointFileUpload: "http://13.68.177.51:8082/ai/",
-  apiEndointFileUploadHcc: "https://local.hcc.encipherhealth.com/secure/",
-  apiLocal: "http://localhost:3003/"
+  apiEndoint: process.env.NEXT_PUBLIC_PORTAL_BASE_URL,
+  apiEndointFileUpload: process.env.NEXT_PUBLIC_PORTAL_FILE_BASE_URL,
+  apiEndointFileUploadHcc:process.env.NEXT_PUBLIC_PORTAL_FILE_HCC_BASE_URL,
+  apiLocal: process.env.NEXT_PUBLIC_LOCAL_PORTAL_BASE_URL
 };
 export default ENDPOINTS;
-  
