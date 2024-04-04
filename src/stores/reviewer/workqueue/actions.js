c3c0183cf366c0997f96fa501f13fd7f8bed8524
Ambika-Innoura <ambika@innoura.com>
2024-04-04 17:34:03 +0530

    add patients list setup

--- src/stores/reviewer/workqueue/actions.js
+++ src/stores/reviewer/workqueue/actions.js
@@ -1,7 +1,7 @@
-import { createActionThunk } from "../../utils/redux";
+import { createActionThunk } from "../../../utils/redux";
 import * as network from "./network";
 
-export const loadDashboard = createActionThunk(
-  "DASHBOARD",
-  network.loadDashboard
+export const patientsAction = createActionThunk(
+  "PATIENTS_LIST",
+  network.patientsList
 );
