200c966f80e87e7f2b36b5a149fdc1339978dbef
Ambika-Innoura <ambika@innoura.com>
2024-04-08 12:59:23 +0530

    add validation

--- src/pages/admin/user/index.js
+++ src/pages/admin/user/index.js
@@ -103,58 +103,40 @@ const UserList = () => {
     event.preventDefault();
     const form = event.currentTarget;
 
-    if (formData.userName.includes("@")) {
-      setErrors({
-        ...errors,
-        email: "Username cannot be an email address",
-      });
-      return;
-    }
-
-    if (formData.password !== formData.confirmPassword) {
-      setErrors({
-        ...errors,
-        confirmPass: "Passwords do not match",
-      });
-      return;
-    }
-
+    // const passValidation = getValidatePassword(
+    //   formData?.password,
+    //   setErrors,
+    //   setIsLoading
+    // );
+    // const isConfirmPasswordValid = validateConfirmPassword(
+    //   formData.password,
+    //   formData.confirmPassword,
+    //   setErrors,
+    //   setIsLoading
+    // );
     if (form.checkValidity() === true) {
-      const passValidation = getValidatePassword(
-        formData?.password,
-        setErrors,
-        setIsLoading
-      );
-      const isConfirmPasswordValid = validateConfirmPassword(
-        formData.password,
-        formData.confirmPassword,
-        setErrors,
-        setIsLoading
-      );
+      formData.tenantId = localTenantId;
+      formData.organizationId = localOrgId;
+      formData.role = roleValue ? roleValue : [role.toUpperCase()];
 
-      if (passValidation && isConfirmPasswordValid) {
-        formData.tenantId = localTenantId;
-        formData.organizationId = localOrgId;
-        formData.role = roleValue ? roleValue : [role.toUpperCase()];
+      // const response = await AddUser(formData, setErrors);
 
-        const response = await AddUser(formData, setErrors);
-
-        if (response?.data?.status === "SUCCESS") {
-          setAddUser(false);
-          setUseAdd(true);
-          setFormData({
-            ...intialValues,
-            userName: "",
-            confirmPassword: "",
-          });
-          setErrors({
-            email: "",
-            password: "",
-            confirmPass: "",
-          });
-          setIsLoadingBtn(false);
-        }
-      }
+      // if (response?.data?.status === "SUCCESS") {
+      //   setAddUser(false);
+      //   setUseAdd(true);
+      //   setFormData({
+      //     ...intialValues,
+      //     userName: "",
+      //     confirmPassword: "",
+      //   });
+      //   setErrors({
+      //     email: "",
+      //     password: "",
+      //     confirmPass: "",
+      //   });
+      //   setIsLoadingBtn(false);
+      // }
+      console.log(formData);
     }
 
     setValidated(true);
@@ -260,6 +242,25 @@ const UserList = () => {
       setFormData(intialValues);
     }, 650);
   }, [addUser]);
+  const getValidatePassword = (formData) => {
+    const passwordRegex =
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#])[A-Za-z\d@$!%*?&#]{8,}$/;
+
+    if (formData?.password?.length === 0) return "Please enter password";
+    if (formData?.password?.length < 8)
+      return "Password should be greater than 8 characters";
+    if (formData?.password?.length > 14)
+      return "Password should be less than 14 characters";
+    if (
+      formData?.password?.length > 0 &&
+      !passwordRegex.test(formData?.password)
+    )
+      return "Password must contain at least 1 capital letter, 1 small letter, 1 number, and 1 special character";
+    else {
+      return "";
+    }
+  };
+
   return (
     <>
       <div className={`show ${sideMenu ? "menu-toggle" : ""}`}>
@@ -464,6 +465,15 @@ const UserList = () => {
                       onChange={handleChange}
                       placeholder="Enter First Name"
                     />
+                    {validated ? (
+                      <div className="text-danger fs-12">
+                        {formData?.firstName?.length === 0
+                          ? "Please enter the firstname"
+                          : ""}
+                      </div>
+                    ) : (
+                      ""
+                    )}
                   </div>
                   <div className="col-xl-6 mb-3">
                     <Form.Label>
@@ -476,6 +486,15 @@ const UserList = () => {
                       onChange={handleChange}
                       placeholder="Enter Last Name"
                     />
+                    {validated ? (
+                      <div className="text-danger fs-12">
+                        {formData?.lastName?.length === 0
+                          ? "Please enter the lastname"
+                          : ""}
+                      </div>
+                    ) : (
+                      ""
+                    )}
                   </div>
                   <div className="col-xl-6 mb-3">
                     <Form.Label>
@@ -488,6 +507,17 @@ const UserList = () => {
                       onChange={handleChange}
                       placeholder="Enter Email"
                     />
+                    {validated ? (
+                      <div className="text-danger fs-12">
+                        {formData?.emailId?.length === 0
+                          ? "Please enter the email"
+                          : !formData.emailId.includes("@")
+                          ? "Please enter valid email"
+                          : ""}
+                      </div>
+                    ) : (
+                      ""
+                    )}
                   </div>
                   <div className="col-xl-6 mb-3">
                     <Form.Label>
@@ -504,8 +534,14 @@ const UserList = () => {
                         value={formData?.userName}
                       />
                     </div>
-                    {errors?.email ? (
-                      <div className="text-danger fs-12">{errors?.email}</div>
+                    {validated ? (
+                      <div className="text-danger fs-12">
+                        {formData?.userName?.length === 0
+                          ? "Please enter the username"
+                          : formData.userName.includes("@")
+                          ? "Username shopuld not contain @ symbol"
+                          : ""}
+                      </div>
                     ) : (
                       ""
                     )}
@@ -522,6 +558,18 @@ const UserList = () => {
                       placeholder="Enter Mobile Number"
                       value={formData?.mobileNumber}
                     />
+                    {validated ? (
+                      <div className="text-danger fs-12">
+                        {formData?.mobileNumber?.length === 0
+                          ? "Please enter the mobileNumber"
+                          : formData?.mobileNumber?.length > 10 ||
+                            formData?.mobileNumber?.length < 10
+                          ? "Please enter valid mobileNumber"
+                          : ""}
+                      </div>
+                    ) : (
+                      ""
+                    )}
                   </div>
                   <div className="col-xl-6 mb-3">
                     <Form.Label>
@@ -547,6 +595,15 @@ const UserList = () => {
                       }
                       required
                     />
+                    {validated ? (
+                      <div className="text-danger fs-12">
+                        {formData?.role?.length === 0
+                          ? "Please select role"
+                          : ""}
+                      </div>
+                    ) : (
+                      ""
+                    )}
                   </div>
 
                   <div className="col-xl-6 mb-3">
@@ -556,7 +613,12 @@ const UserList = () => {
 
                     <div
                       className={styles.passCOntainer}
-                      style={{ border: errors?.password && "1px solid red" }}
+                      style={{
+                        border:
+                          validated &&
+                          formData?.password?.length === 0 &&
+                          "1px solid red",
+                      }}
                     >
                       <div style={{ width: "100%" }}>
                         <Form.Control
@@ -584,9 +646,9 @@ const UserList = () => {
                       </div>
                     </div>
 
-                    {errors?.password ? (
+                    {validated ? (
                       <div className="text-danger fs-12">
-                        {errors?.password}
+                        {getValidatePassword(formData)}
                       </div>
                     ) : (
                       <small id="emailHelp" class="form-text text-muted">
@@ -602,7 +664,12 @@ const UserList = () => {
                     </Form.Label>
                     <div
                       className={styles.passCOntainer}
-                      style={{ border: errors?.password && "1px solid red" }}
+                      style={{
+                        border:
+                          validated &&
+                          formData?.confirmPassword?.length === 0 &&
+                          "1px solid red",
+                      }}
                     >
                       <div style={{ width: "95%" }}>
                         <Form.Control
@@ -628,9 +695,13 @@ const UserList = () => {
                         </span>
                       </div>
                     </div>
-                    {errors?.confirmPass ? (
+                    {validated ? (
                       <div className="text-danger fs-12">
-                        {errors?.confirmPass}
+                        {formData?.confirmPassword?.length === 0
+                          ? "Please enter confirm password"
+                          : formData?.password !== formData?.confirmPassword
+                          ? "Password is not matched"
+                          : ""}
                       </div>
                     ) : (
                       ""
