2ea12c0c08e871f07e9914f10b338eaf4f6a7424
priya-innoura <141149107+priya-innoura@users.noreply.github.com>
2024-04-13 14:15:36 +0530

    removed user select  for sent and received report in the report module

--- src/pages/admin/report/index.js
+++ src/pages/admin/report/index.js
@@ -36,7 +36,7 @@ const statusOptions = [
 
 const index = () => {
   const dispatch = useDispatch();
-  const route = useRouter()
+  const route = useRouter();
   const ExportResponse = useSelector((state) => state.adminReport?.exportRes);
 
   const ReportPatientDetails = useSelector(
@@ -255,21 +255,20 @@ const index = () => {
     const page = new URLSearchParams(window.location.search).get("page");
     const limit = new URLSearchParams(window.location.search).get("limit");
     if (reportActiveTab === "ReceivedReport" && page) {
-      setReceivedPageNo(page)
-      setPaginationReceivedFirst(limit)
+      setReceivedPageNo(page);
+      setPaginationReceivedFirst(limit);
     } else if (reportActiveTab === "SentReport" && page) {
-      setSentPageNo(page)
-      setPaginationSentFirst(limit)
-    } 
-  }, [reportActiveTab])
-
+      setSentPageNo(page);
+      setPaginationSentFirst(limit);
+    }
+  }, [reportActiveTab]);
 
   const backRender = () => {
-    const user = localStorage.getItem('userRole')
-    if (user == 'admin') {
-      route.push('/admin/report?page=0&limit=0')
+    const user = localStorage.getItem("userRole");
+    if (user == "admin") {
+      route.push("/admin/report?page=0&limit=0");
     }
-  }
+  };
 
   return (
     <>
@@ -320,6 +319,7 @@ const index = () => {
                             defaultSelectValue2={selectedCoderOptReport}
                             // selector3
                             isSelector3={
+                              reportActiveTab === "CoderReport" &&
                               selectUserList?.data?.response?.length
                                 ? true
                                 : false
@@ -391,7 +391,7 @@ const index = () => {
                                     className="nav-item"
                                     onClick={() => {
                                       handleTabs("CoderReport");
-                                      backRender()
+                                      backRender();
                                     }}
                                   >
                                     <Nav.Link
@@ -406,7 +406,7 @@ const index = () => {
                                     className="nav-item"
                                     onClick={() => {
                                       handleTabs("SentReport");
-                                      backRender()
+                                      backRender();
                                     }}
                                   >
                                     <Nav.Link
@@ -421,7 +421,7 @@ const index = () => {
                                     className="nav-item"
                                     onClick={() => {
                                       handleTabs("ReceivedReport");
-                                      backRender()
+                                      backRender();
                                     }}
                                   >
                                     <Nav.Link
