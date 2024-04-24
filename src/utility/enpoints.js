7649937d20ac642c8451818bbfc1de903cddebc7
vignesh-innoura <vignesh@innoura.com>
2024-04-24 20:03:45 +0530

    pdfviewr

--- src/utility/enpoints.js
+++ src/utility/enpoints.js
@@ -4,6 +4,6 @@ const ENDPOINTS = {
   apiEndointFileUploadHcc: "https://hcc.encipherhealth.com/secure/",
   apiLocal: "http://localhost:3003/",
   isShow: false,
-  PdfViewer: process.env.NEXT_PUBLIC_PDF_VIEWER_URL
+  PdfViewer: "https://pdffile.javagcai.com/web/viewer.html"
 };
 export default ENDPOINTS;
