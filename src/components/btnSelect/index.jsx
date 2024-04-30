a73f7180e963efa21cf5fc1a472169270878d92e
vignesh-innoura <vignesh@innoura.com>
2024-04-30 20:18:42 +0530

    manually added new form valiation

---
+++ src/components/btnSelect/index.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Style from "./style.module.css";
+
+const SelectButton = ({ select, setSelect, completed }) => {
+  const list = ["M", "E", "A", "T"];
+
+  return (
+    <div className="d-flex my-2">
+      <span className={`d-inline-block ${Style.containerBtn}`}>
+        {list.map((i) => (
+          <div
+            className={`d-inline-block p-2 px-5 ${Style.btns} ${
+              completed.includes(i)
+                ? `bg-success ${Style.textColor} `
+                : select == i
+                ? `${Style.btnColor}`
+                : ""
+            }`}
+            onClick={() => setSelect(i)}
+          >
+            {i}
+          </div>
+        ))}
+      </span>
+    </div>
+  );
+};
+
+export default SelectButton;
