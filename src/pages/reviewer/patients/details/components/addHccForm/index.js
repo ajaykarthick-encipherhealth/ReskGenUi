801cd4dfbbf77bb2017f228c3710c819f2ed9499
Ajith <ajith@innoura.com>
2024-04-30 17:01:57 +0530

    issue fix

--- src/pages/reviewer/patients/details/components/addHccForm/index.js
+++ src/pages/reviewer/patients/details/components/addHccForm/index.js
@@ -283,6 +283,8 @@ const AddHccForm = ({
                   onChange={getFindNpiNumber}
                   maxLength={10}
                   className={styles.formControl}
+                  onWheel={(e) => e.target.blur()}
+
                 />
               </Form.Item>
             )}
