f4806c8bd91c489edace81606a66fe2c2a60b302
vignesh-innoura <vignesh@innoura.com>
2024-05-07 19:53:49 +0530

    change count

--- src/pages/physicians/dashboard/doubleBarChant/index.js
+++ src/pages/physicians/dashboard/doubleBarChant/index.js
@@ -10,40 +10,50 @@ const DoubleBarChant = ({ data }) => {
     dataset: {
       dimensions: ["product", "2016", "2017"],
       source: [
-        { product: "ICD10", 2016: 85.8, 2017: 93.7 },
-        { product: "ICD11", 2016: 73.4, 2017: 55.1 },
-        { product: "ICD13", 2016: 65.2, 2017: 82.5 },
-        { product: "ICD14", 2016: 53.9, 2017: 39.1 },
-        { product: "ICD15", 2016: 85.8, 2017: 93.7 },
-        { product: "ICD16", 2016: 73.4, 2017: 55.1 },
-        { product: "ICD17", 2016: 65.2, 2017: 82.5 },
-        { product: "ICD18", 2016: 53.9, 2017: 39.1 },
-        { product: "ICD19", 2016: 85.8, 2017: 93.7 },
-        { product: "ICD12", 2016: 73.4, 2017: 55.1 },
-        { product: "ICD22", 2016: 65.2, 2017: 82.5 },
-        { product: "ICD23", 2016: 53.9, 2017: 39.1 },
-        { product: "ICD", 2016: 85.8, 2017: 93.7 },
-        { product: "ICD0", 2016: 73.4, 2017: 55.1 },
-        { product: "ICD2", 2016: 65.2, 2017: 82.5 },
-        { product: "ICD6", 2016: 53.9, 2017: 39.1 },
+        { product: "ICD10", 2016: 8, 2017: 3 },
+        { product: "ICD11", 2016: 7, 2017: 4 },
+        { product: "ICD13", 2016: 6, 2017: 2 },
+        { product: "ICD14", 2016: 9, 2017: 5 },
+        { product: "ICD15", 2016: 8, 2017: 3 },
+        { product: "ICD16", 2016: 7, 2017: 4 },
+        { product: "ICD17", 2016: 6, 2017: 2 },
+        { product: "ICD18", 2016: 9, 2017: 5 },
+        { product: "ICD19", 2016: 8, 2017: 3 },
+        { product: "ICD12", 2016: 7, 2017: 4 },
+        { product: "ICD22", 2016: 6, 2017: 2 },
+        { product: "ICD23", 2016: 9, 2017: 5 },
+        { product: "ICD", 2016: 8, 2017: 3 },
+        { product: "ICD0", 2016: 7, 2017: 4 },
+        { product: "ICD2", 2016: 6, 2017: 2 },
+        { product: "ICD6", 2016: 9, 2017: 5 },
       ],
     },
     xAxis: { type: "category" },
-    yAxis: {},
+    yAxis: {
+      show: false
+    },
     // Declare several bar series, each will be mapped
     // to a column of dataset.source by default.
     series: [
       {
         type: "bar",
         itemStyle: {
-          color: "green", // Color for the first bar series
+          color: "#00ab00", // Color for the first bar series
         },
+        label: {
+          show: true,
+          position: 'top' // Display count at the top
+        }
       },
       {
         type: "bar",
         itemStyle: {
-          color: "red", // Color for the first bar series
+          color: "#fe5b5b", // Color for the first bar series
         },
+        label: {
+          show: true,
+          position: 'top' // Display count at the top
+        }
       },
     ],
   };
