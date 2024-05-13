0d9df2ea1f83c38721124ec3b0f7616a826a1f62
Ambika-Innoura <ambika@innoura.com>
2024-05-13 13:39:13 +0530

    chnages added

--- src/components/patientDetails/details/components/model/Index.js
+++ src/components/patientDetails/details/components/model/Index.js
@@ -27,6 +27,7 @@ const ModelIndex = ({
   initialValues,
   setInitialValues,
   setOpenContent,
+  selectedData
 }) => {
   const [form] = Form.useForm();
   const { TextArea } = Input;
@@ -131,6 +132,7 @@ const ModelIndex = ({
           initialValues={initialValues}
           setInitialValues={setInitialValues}
           setOpenContent={setOpenContent}
+          selectedData={selectedData}
         />
       )}
     </Modal>
