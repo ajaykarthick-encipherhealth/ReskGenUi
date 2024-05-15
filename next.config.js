a52f7dde0292f128e3aa6945ab984e48032ab2f1
vignesh-innoura <vignesh@innoura.com>
2024-05-15 20:15:31 +0530

    secuity changes

--- next.config.js
+++ next.config.js
@@ -3,6 +3,19 @@ const nextConfig = {
   reactStrictMode: false,
   // useFileSystemPublicRoutes:true,
   poweredByHeader: false,
+  async headers() {
+    return [
+      {
+        source: '/(.*)', // Apply this header to all routes
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+        ],
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
