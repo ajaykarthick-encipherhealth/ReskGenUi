e6a3d3e384ac408e484516bd8414b4f0897302c1
priya-innoura <141149107+priya-innoura@users.noreply.github.com>
2024-05-18 14:21:44 +0530

    added svg icon

--- src/mainStream/components/flagCount/index.js
+++ src/mainStream/components/flagCount/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { SVGICON } from "../../../jsx/constant/theme";
 
 const Flags = ({ flagsData, styles }) => {
   return (
@@ -8,19 +9,7 @@ const Flags = ({ flagsData, styles }) => {
         {flagsData?.map((flagItem) => (
           <div className={styles.contentGroups} key={flagItem.id}>
             <div>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="23"
-                height="23"
-                viewBox="0 0 800 800"
-                fill={flagItem?.flagColour}
-              >
-                <path
-                  d="M223 100V102H225H696.392L573.304 298.94L572.642 300L573.304 301.06L696.392 498H225H223V500V748H152V52H223V100Z"
-                  stroke="#000"
-                  strokeWidth="10"
-                />
-              </svg>
+            {SVGICON.flagsSvg}
               <span style={{ fontSize: "12px" }}>
                 {flagItem?.flagName ? flagItem?.flagName.replaceAll("_", " ") : ""}
               </span>
