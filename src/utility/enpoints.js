fbd050892ed1be2a750c024a7a40d427b5d9752a
Ajith <ajith@innoura.com>
2024-05-21 15:00:41 +0530

    local api end point changed

--- src/utility/enpoints.js
+++ src/utility/enpoints.js
@@ -1,7 +1,7 @@
 const ENDPOINTS = {
-  apiEndoint: "https://hcc.encipherhealth.com/secure/",
+  apiEndoint: "https://local.hcc.encipherhealth.com/secure/",
   apiEndointFileUpload: "http://13.68.177.51:8082/ai/",
-  apiEndointFileUploadHcc: "https://hcc.encipherhealth.com/secure/",
+  apiEndointFileUploadHcc: "https://local.hcc.encipherhealth.com/secure/",
   apiLocal: "http://localhost:3003/",
   isShow: false,
   PdfViewer: "https://viewer.cogentai.encipherhealth.com/web/viewer.html",
