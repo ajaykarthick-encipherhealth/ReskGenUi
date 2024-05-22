5e6d587db16425c5e615abd629f384d0e199f497
Ajith <ajith@innoura.com>
2024-05-22 13:45:24 +0530

    api hide

--- src/components/patientDetails/details/components/function/GetDataLab.js
+++ src/components/patientDetails/details/components/function/GetDataLab.js
@@ -119,17 +119,17 @@ export const getPatientLabDetails = async (
           });
         });
 
-        var sectionColorResult = sectionColorList.result?.response;
+        // var sectionColorResult = sectionColorList.result?.response;
 
-        let sectionColorResultMatch = sectionColorResult.filter((o1) =>
-          dublicateSectionArr.some((o2) => o1.sectionName === o2.name)
-        );
-        let sectionColorResultNotMatch = dublicateSectionArr.filter(
-          (o1) => !sectionColorResult.some((o2) => o1.name === o2.sectionName)
-        );
+        // let sectionColorResultMatch = sectionColorResult.filter((o1) =>
+        //   dublicateSectionArr.some((o2) => o1.sectionName === o2.name)
+        // );
+        // let sectionColorResultNotMatch = dublicateSectionArr.filter(
+        //   (o1) => !sectionColorResult.some((o2) => o1.name === o2.sectionName)
+        // );
 
         var notMatchColorArray = [];
-        sectionColorResultNotMatch?.map((res, index) => {
+        dublicateSectionArr?.map((res, index) => {
           var radomColorcode = stringToColour(res.name);
           var randomColorChangeShadow = radomColorcode + 33;
           notMatchColorArray.push({
@@ -146,8 +146,8 @@ export const getPatientLabDetails = async (
 
         var newArrayColorMatchs = [];
         newArrayColorMatchs = [
-          ...sectionColorResult,
-          ...sectionColorResultMatch,
+          // ...sectionColorResult,
+          // ...sectionColorResultMatch,
           ...notMatchColorArray,
         ];
 
--- src/components/patientDetails/details/index.js
+++ src/components/patientDetails/details/index.js
@@ -239,8 +239,8 @@ const Details = ({ workFgetFlagsowData, getFlagsData }) => {
 
   useEffect(() => {
     getAllProcessYear();
-    const patientId = localStorage.getItem("patientId");
-    dispatch(getAllSectionColor());
+    // const patientId = localStorage.getItem("patientId");
+    // dispatch(getAllSectionColor());
     // dispatch(
     //   getPatientDetailsResult(
     //     selectPatientId ? selectPatientId?.patirntId : patientId
@@ -304,15 +304,15 @@ const Details = ({ workFgetFlagsowData, getFlagsData }) => {
       setDosYearDefalutSelect(dosYearArr[0]);
       setSelectedDosValue(dosYearArr[0].value);
       setDosYear(dosYearArr);
-      setIsLoadingDos(false);
-        dispatch(
-          getPatientDosList(
-            selectPatientId ? selectPatientId?.patirntId : patientId,
-            dosYearArr[0].value
-          )
-        );
+      setIsLoadingDos(false);       
       dispatch(
         getPatientDetailsResultNew(
+          selectPatientId ? selectPatientId?.patirntId : patientId,
+          dosYearArr[0].value
+        )
+      );
+      dispatch(
+        getPatientDosList(
           selectPatientId ? selectPatientId?.patirntId : patientId,
           dosYearArr[0].value
         )
@@ -1958,9 +1958,9 @@ const Details = ({ workFgetFlagsowData, getFlagsData }) => {
                 </div>
               </div>
             ) : null}
-            {sectionColorList?.loading == true ? (
+            {/* {sectionColorList?.loading == true ? (
               <SpinnerDots />
-            ) : (
+            ) : ( */}
               <div
                 className={`container-fluid ${visitStyles.container_fluid_patient}`}
               >
@@ -3380,7 +3380,7 @@ const Details = ({ workFgetFlagsowData, getFlagsData }) => {
                   ) : null}
                 </div>
               </div>
-            )}
+            {/* )} */}
             {/* <Footer/> */}
           </div>}
         </div>
