57ac0f54305660cb6f806ed1afbef21a391d6901
Ajith <ajith@innoura.com>
2024-06-03 16:18:48 +0530

    null issue check

--- src/components/patientDetails/details/hcc/raf/index.js
+++ src/components/patientDetails/details/hcc/raf/index.js
@@ -180,7 +180,7 @@ const RafScore = ({ patientDetailsResult }) => {
                     <div className="row">
                       <div className="col-xl-6">V28 score</div>
                       <div className="col-xl-6">
-                        V28Score({rafScoreList?.rafVersionDTO.v28Percentage}%)
+                        V28Score({rafScoreList?.rafVersionDTO?.v28Percentage}%)
                       </div>
                     </div>
                   </div>
