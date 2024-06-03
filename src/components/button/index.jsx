565b2c77c3c2e4a6e227f7c1e2c7586e65287e7d
Ambika-Innoura <ambika@innoura.com>
2024-06-03 17:22:24 +0530

    removed log

--- src/components/button/index.jsx
+++ src/components/button/index.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Style from "./style.module.css";
 
 const RegularButton = ({ type, name, onClick, width, method,loading, disabled,htmlType }) => {
-  console.log(htmlType)
   return (
     <button
       className={`btn mx-1 ${
@@ -13,7 +12,7 @@ const RegularButton = ({ type, name, onClick, width, method,loading, disabled,ht
       style={{ width: width }}
       type={method == "reset" ? "reset" : "submit"}
       disabled={disabled}
-      htmlType={htmlType ?htmlType:""}
+      htmlType={htmlType}
     >
       {loading?"Loading...":name}
     </button>
