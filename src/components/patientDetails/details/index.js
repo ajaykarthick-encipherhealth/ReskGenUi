4402d696810b1529fd8d0271d57540ad97b7b4b6
Ajith <ajith@innoura.com>
2024-06-04 10:46:37 +0530

    overall score key changed

--- src/components/patientDetails/details/index.js
+++ src/components/patientDetails/details/index.js
@@ -721,9 +721,9 @@ const Details = ({
                       >
                         <div className={`${visitStyles.rafscoreheader} `}>
                           <label>Score</label>
-                          {patientDetails.rafScore?.rafVersionDTO?.overAllScore != null ? (
+                          {patientDetails?.rafScore?.rafVersionDTO?.overAllScore != null ? (
                             <h6 className="ageDtails">
-                              {patientDetails.rafScore?.rafVersionDTO?.overAllScore?.toFixed(3)}
+                              {patientDetails?.rafScore?.rafVersionDTO?.overAllScore?.toFixed(3)}
                             </h6>
                           ) : (
                             <h6 className="ageDtails">0.00</h6>
