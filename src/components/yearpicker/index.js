83583301f4519ab9a1b327a5b4948d51f97da9ea
priya-innoura <141149107+priya-innoura@users.noreply.github.com>
2024-06-07 12:18:49 +0530

    dashboard and report issue fix

--- src/components/yearpicker/index.js
+++ src/components/yearpicker/index.js
@@ -23,6 +23,7 @@ const YearPicker = ({
         <DatePicker
           onChange={onChangeYear}
           picker={"year"}
+          allowClear
           value={dayjs(val1 ? val1 : currentYearDate, "YYYY")}
           format={"YYYY"}
           className={`${styles.picker} pickerChnages`}
