b12f161d87cbe7a474e05ab932f65399a3dc851c
priya-encipher <priya@encipherhealth.com>
2024-06-18 14:11:46 +0530

    replaced codify icon

--- src/jsx/layouts/nav/Header.js
+++ src/jsx/layouts/nav/Header.js
@@ -25,9 +25,11 @@ import {
   SettingOutlined,
   CloseOutlined,
   CreditCardOutlined,
+  BookOutlined,
 } from "@ant-design/icons";
+
 import styles from "../../../styles/file-managemnt.module.css";
-import { IMAGES } from "../../constant/theme";
+import { IMAGES, SVGICON } from "../../constant/theme";
 import {
   AdminMenuList,
   PhysicanMenuList,
@@ -516,10 +518,9 @@ const Header = ({
                     <div className="nav-link i-false" as="div">
                       <div className="header-info2 d-flex align-items-center">
                         <div className={styles.codify}>
-                          <CreditCardOutlined
-                            className={styles.lapicon}
-                            onClick={showDrawer}
-                          />
+                          <div onClick={showDrawer}>
+                          {SVGICON.codify}
+                          </div>
                         </div>
                         <Drawer
                           title="CODES"
