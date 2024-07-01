c79a6091ec097b8dff27241a6a383a95fd37199c
ambika-encipher <ambika@encipherhealth.com>
2024-07-01 19:31:35 +0530

    chnaged raf key

--- src/mainStream/reports/initialReport/index.js
+++ src/mainStream/reports/initialReport/index.js
@@ -215,11 +215,11 @@ const InitialCard = ({
     },
     {
       title: "Avg RAF score",
-      value: reportListAll?.rafAverage?.toFixed(4),
+      value: reportListAll?.totalRafScore?reportListAll?.totalRafScore?.toFixed(4):0,
     },
     {
       title: "HCC Count",
-      value: reportListAll?.totalHccCount,
+      value: reportListAll?.totalHccCount?reportListAll?.totalHccCount:0,
     },
   ];
   const allocationCountData = [
