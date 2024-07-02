e755920ac3391d9e648b88109d6cc418bcc929f5
ambika-encipher <ambika@encipherhealth.com>
2024-07-02 18:49:25 +0530

    chnaged data

--- src/stores/tenantAdmin/network.js
+++ src/stores/tenantAdmin/network.js
@@ -101,8 +101,6 @@ export async function getAllTracking(datas) {
       datas?.pageNo
     }&size=15&processedStatus=${filteredStatus}&processedStart=${filteredDStart}&processedEnd=${
       datas?.dueDateEnd
-    }&auditedStartDate=${datas?.processedStart}&auditedEndDate=${
-      datas?.processedEnd
     }&searchString=${datas?.searchTextValue}&patientAllocated=${
       datas?.selAllocatedTo === "All" ? "" : datas?.selAllocatedTo
     }&auditAllocatedStart=${datas?.allocatedStartDate}&auditAllocatedEnd=${
