b658660abe3c04109d7c3c8882a7ec4d058c918e
ambika-encipher <ambika@encipherhealth.com>
2024-07-02 11:47:35 +0530

    changes added in admin accuracy

--- src/pages/admin/dashboard/completedStatus/index.js
+++ src/pages/admin/dashboard/completedStatus/index.js
@@ -212,7 +212,7 @@ const CompletedStatus = () => {
 
   return (
     <>
-      <HeadTitle header="Completed Status" />
+      <HeadTitle header="Production Insights" />
       <div className={styles.card5}>
         <Card borderRadius="28px" padding="10px">
           <div className={styles.buttonDiv}>
