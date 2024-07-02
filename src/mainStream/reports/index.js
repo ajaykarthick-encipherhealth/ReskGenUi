ed92169e51162a02519abcfda2a71b487bc2ce11
ambika-encipher <ambika@encipherhealth.com>
2024-07-02 15:55:12 +0530

    add pagenum

--- src/mainStream/reports/index.js
+++ src/mainStream/reports/index.js
@@ -338,6 +338,7 @@ const Reports = ({
     } else if (activeTab === "Admin") {
       dispatch(
         getReportDetails({
+          pagenum: pageNo,
           startDate: selectedDateRanges?.Admin?.from,
           endDate: selectedDateRanges?.Admin?.to,
           search: coderSearchString ? coderSearchString : "",
@@ -482,7 +483,9 @@ const Reports = ({
                             </div>
                           </div>
 
-                          {!activeTab || activeTab === "Reviewer" || userRole == "supervisor"  ? (
+                          {!activeTab ||
+                          activeTab === "Reviewer" ||
+                          userRole == "supervisor" ? (
                             <div className="col-xl-2">
                               <div className="d-flex w-100">
                                 <label className="labelStyle d-flex m-auto  p-2">
