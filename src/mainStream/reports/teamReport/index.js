0c7fea1c1e626b751904fdfd71c0a9bebe908b6f
Priya-encpherhealth <priya@encipherhealth.com>
2024-07-16 13:34:27 +0530

    nodata issue fixed in supervisor report module

--- src/mainStream/reports/teamReport/index.js
+++ src/mainStream/reports/teamReport/index.js
@@ -342,8 +342,10 @@ const TeamReport = ({
                 <div>
                   <div className=" col-xl-12 d-flex" style={{ height: "100%" }}>
                     {reportListAll?.response?.response?.data?.length === 0 ? (
-                      <div className={`col-xl-12 ${styles.emptyCard}`}>
+                       <div className={`col-xl-6 ${styles.card1}`}>
+                      <div className={` ${styles.emptyCard}`}>
                         <Empty />
+                      </div>
                       </div>
                     ) : (
                       <div className={`col-xl-6 ${styles.cardDiv}`}>
