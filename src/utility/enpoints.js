ba437b12b8c921b61f0c74c5b69deadf30bf4482
vignesh-innoura <vignesh@innoura.com>
2024-07-18 17:23:07 +0530

    enpoint chnaged

--- src/utility/enpoints.js
+++ src/utility/enpoints.js
@@ -1,7 +1,7 @@
 const ENDPOINTS = {
-  apiEndoint: "https://local.hcc.encipherhealth.com/secure/",
+  apiEndoint: "https://hcc.encipherhealth.ai/secure/",
   apiEndointFileUpload: "http://13.68.177.51:8082/ai/",
-  apiEndointFileUploadHcc: "https://local.hcc.encipherhealth.com/secure/",
+  apiEndointFileUploadHcc: "https://hcc.encipherhealth.ai/secure/",
   apiLocal: "http://localhost:3003/",
   isShow: false,
   PdfViewer: "https://viewer.cogentai.encipherhealth.com/web/viewer.html",
