b48ba3ea750f9eadee07715a72ad484cb983bb77
ambika-encipher <ambika@encipherhealth.com>
2024-07-18 12:43:17 +0530

    disable  progress

--- src/components/table/admin/FileProcessing/FileProcessing.js
+++ src/components/table/admin/FileProcessing/FileProcessing.js
@@ -557,6 +557,7 @@ const FileProcessingTable = ({
                 marginTop: stepperVisible ? "10px" : "0",
                 marginLeft: "-40px",
               }}
+              className={errStages[data?.processStageChart]?"errStages":""}
               
             >
               <Steps
@@ -567,8 +568,9 @@ const FileProcessingTable = ({
                 }
                 labelPlacement="vertical"
                 items={mappedSteps}
-                percent={failedList ? 0 : count}
+                percent={failedList|| errStages[data?.processStageChart] ? 0 : count}
                 finishIconBorderColor="#000"
+                // className={errStages[data?.processStageChart]?"errStages":""}
               />
             </div>
           </>
