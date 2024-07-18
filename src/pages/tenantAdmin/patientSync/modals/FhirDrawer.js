35a1178afd8700c2cc5d96d3da50b47fff6e33f1
ambika-encipher <ambika@encipherhealth.com>
2024-07-18 16:01:18 +0530

    batchupload

--- src/pages/tenantAdmin/patientSync/modals/FhirDrawer.js
+++ src/pages/tenantAdmin/patientSync/modals/FhirDrawer.js
@@ -12,7 +12,16 @@ import ENDPOINTS from "../../../../utility/enpoints";
 import axios from "../../../../utility/axiosConfig";
 import Image from "next/image";
 import style from "../fhir.module.css";
+import sampleFiles from '../sampleFiles/sampleCsv.csv'
 
+export const getYears = () => {
+  const currentYear = new Date().getFullYear();
+  let year = [];
+  for (let i = 2016; i < currentYear + 1; i++) {
+    year.push({ label: i, value: i });
+  }
+  return year;
+};
 const inputTypeOptions = [
   { label: "Excel", value: "EXCEL" },
   { label: "Csv", value: "CSV" },
@@ -24,70 +33,84 @@ const FhirDrawer = ({
   isDrawerOpen,
   setIsDrawerOpen,
   uploadType,
-  fileList,
-  setFileList,
-  // getCreateBatch,
+  Batch,
   // getAllBatches,
   selectedBatch,
   setSelectedBatch,
 }) => {
   const [form] = Form.useForm();
+  const [fileList, setFileList] = useState([]);
+  const [selectedType, setSelectedType] = useState(null);
   const handleClose = (form) => {
+    setFileList([]);
+    setSelectedType(null);
     setIsDrawerOpen(false);
     form.resetFields();
     if (setSelectedBatch) {
       setSelectedBatch();
     }
   };
-  const reportActiveTab = useSelector((state) => state.AuditReport?.activetab);
 
-  const handleFileUpload = async () => {
-    try {
-      const uploadPromises = fileList.map((item) => {
-        const formData = new FormData();
-        formData.append("file", item);
-        formData.append("batchId", selectedBatch?.id);
-        formData.append("yearOfServices", selectedBatch?.yearOfService);
-
-        const headers = {
-          headers: {
-            "Content-Type": "multipart/form-data",
-          },
-        };
-
-        // return axios.post(
-        //   `${ENDPOINTS.apiEndoint}management/batch/upload`,
-        //   formData,
-        //   headers
-        // );
-      });
-
-      // const responses = await Promise.all(uploadPromises);
-      // responses.forEach((res) => {
-      //   if (res.data.status === "SUCCESS") {
-      //     form.resetFields();
-      //   }
-      // });
-      setIsDrawerOpen(false);
-    } catch (err) {
-      console.error("Error uploading files:", err);
-    }
-  };
-
-  const onFinish = async (formVal) => {
-    if (reportActiveTab === "PDF") {
-      if (uploadType !== "upload") {
-        const res = await getCreateBatch({ info: formVal });
-        if (res.status === "SUCCESS") {
-          await getAllBatches({ page: 0 });
-          form.resetFields();
-        }
-      } else {
-        handleFileUpload();
+  const generateUUID = () => {
+    let dt = new Date().getTime();
+    let uuid = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(
+      /[xy]/g,
+      function (c) {
+        let r = (dt + Math.random() * 16) % 16 | 0;
+        dt = Math.floor(dt / 16);
+        return (c === "x" ? r : (r & 0x3) | 0x8).toString(16);
       }
+    );
+    return uuid;
+  };
+  const onFinish = async (formVal) => {
+    const newUuid = generateUUID();
+    // const formData = new FormData();
+    // formData.append("file", fileList[0]);
+    // formData.append("batchId", newUuid);
+    // formData.append("batchName", formVal?.batchName);
+    // formData.append("yearOfService", formVal?.yearOfService);
+    // formData.append("batchType", "FHIR");
+    const headers = {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    };
+    const data = {
+      batchId: newUuid,
+      batchName: formVal?.batchName,
+      yearOfService: formVal?.yearOfService,
+      batchType: "FHIR",
+      groupId: formVal?.groupId,
+    };
+    try {
+      const res = axios.post(
+        `${ENDPOINTS.apiEndoint}management/batch/upload`,
+        selectedType === "GROUP_ID" ? data : { ...data, file: fileList[0] },
+        selectedType !== "GROUP_ID" && headers
+      );
+      console.log(res);
+      form.resetFields();
+    } catch (err) {
+      form.resetFields();
     }
   };
-
+  const handleDownload = () => {
+    let fileName;
+    if (selectedType === "CSV") {
+      fileName = "sample.csv";
+    } else if (selectedType === "JSON") {
+      fileName = "sample.json";
+    } else {
+      fileName = "sample.xlsx";
+    }
+    const link = document.createElement('a');
+    link.href = `/${fileName}`;
+    link.download = fileName;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
   return (
     <Offcanvas show={isDrawerOpen} className="offcanvas-end" placement="end">
       <Offcanvas.Header closeButton onClick={() => handleClose(form)}>
@@ -105,7 +128,7 @@ const FhirDrawer = ({
                   Batch Name <span className="text-danger">*</span>{" "}
                 </label>
               }
-              name="name"
+              name="batchName"
               rules={[
                 {
                   required: true,
@@ -113,24 +136,7 @@ const FhirDrawer = ({
                 },
               ]}
             >
-              <Input name="batchid" />
-            </Form.Item>
-
-            <Form.Item
-              label={
-                <label>
-                  Batch ID <span className="text-danger">*</span>{" "}
-                </label>
-              }
-              name="batchID"
-              rules={[
-                {
-                  required: true,
-                  message: "Please Enter Batch ID ",
-                },
-              ]}
-            >
-              <Input name="batchID" maxLength={10} />
+              <Input name="batchName" />
             </Form.Item>
 
             <Form.Item
@@ -148,44 +154,96 @@ const FhirDrawer = ({
               ]}
             >
               <Select
-                mode="tags"
                 name="inputType"
                 style={{ width: "100%" }}
                 options={inputTypeOptions}
                 size="large"
+                onChange={(value) => {
+                  setSelectedType(value);
+                }}
               />
             </Form.Item>
             <Form.Item
               label={
                 <label>
-                  Upload File <span className="text-danger">*</span>
+                  YearOf Service <span className="text-danger">*</span>{" "}
                 </label>
               }
-              name="upload"
+              name="yearOfService"
               rules={[
                 {
                   required: true,
-                  message: "Please Upload File",
+                  message: "Please Enter yearOfService ",
                 },
               ]}
             >
-              <UploadFile
-                filelList={fileList}
-                setFileList={setFileList}
-                uploaderImg={uploaderImg}
-                subText="Upload Excel, CSV, Json & Drag and drop your files"
+              <Select
+                mode="tags"
+                name="yearOfService"
+                style={{ width: "100%" }}
+                options={getYears()}
+                size="large"
               />
             </Form.Item>
-            <Form.Item label={<label>Sample File Input</label>}>
+            {selectedType === "GROUP_ID" ? (
+              <Form.Item
+                label={
+                  <label>
+                    Group ID <span className="text-danger">*</span>{" "}
+                  </label>
+                }
+                name="groupId"
+                rules={[
+                  {
+                    required: true,
+                    message: "Please Enter Group Id ",
+                  },
+                ]}
+              >
+                <Input name="groupId" />
+              </Form.Item>
+            ) : (
+              <>
+              <Form.Item
+                label={
+                  <label>
+                    Upload File <span className="text-danger">*</span>
+                  </label>
+                }
+                name="upload"
+                rules={[
+                  {
+                    required: false,
+                    message: "Please Upload File",
+                  },
+                ]}
+              >
+                <UploadFile
+                  filelList={fileList}
+                  setFileList={setFileList}
+                  uploaderImg={uploaderImg}
+                  subText={
+                    fileList?.length > 0
+                      ? fileList[0]?.name
+                      : "Upload Excel, CSV, Json or Drag and drop your files"
+                  }
+                />
+              </Form.Item>
+              <Form.Item label={<label>Sample File Input</label>}>
               <Button type="button" className={style.sampleBtnStyle}>
-                Sample excel.xlsx
+                {selectedType==="CSV"?"Sample Csv File.csv":selectedType==="JSON"?"Sample Json File.json":"Sample Excel File.xlsx"}
                 <Image
                   src={downloadImg}
                   alt="noImage"
                   className={style.sampleFileImg}
+                  onClick={()=>{handleDownload()}}
                 />
               </Button>
             </Form.Item>
+            </>
+            )}
+
+            
 
             <Form.Item>
               <div className="col-xl-12 mb-3 d-grid justify-content-center">
