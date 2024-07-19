dfeb45b732762d6fd3c33d3a7291f74bcc9e499c
ambika-encipher <ambika@encipherhealth.com>
2024-07-19 10:41:53 +0530

    removed unwanted code

--- src/components/customSelect/index.js
+++ src/components/customSelect/index.js
@@ -2,7 +2,14 @@ import React, { useEffect, useRef, useState } from "react";
 import { PlusOutlined } from "@ant-design/icons";
 import { Button, Divider, Input, Select, Space } from "antd";
 let index = 0;
-const CustomSelect = ({ options, onChange, setOptions, value, disabled,placeholder }) => {
+const CustomSelect = ({
+  options,
+  onChange,
+  setOptions,
+  value,
+  disabled,
+  placeholder,
+}) => {
   //   const [items, setItems] = useState([]);
   const [name, setName] = useState("");
   const inputRef = useRef(null);
@@ -19,51 +26,47 @@ const CustomSelect = ({ options, onChange, setOptions, value, disabled,placehold
       }, 0);
     }
   };
-  const stopPropagation = (e) => {
-    e.stopPropagation();
-  };
+
   return (
     <Select
-          disabled={disabled}
-          size="large"
-          placeholder={placeholder ? placeholder : ""}
-          dropdownRender={(menu) => (
-            <>
-              {menu}
-              <Divider style={{ margin: "8px 0" }} />
-              <div
-                style={{ padding: "0 8px 4px", width: "100%" }}
-                className="d-flex"
-                // onClick={stopPropagation}
-                // onKeyDown={stopPropagation}
-              >
-                <Input
-                  placeholder="Please enter item"
-                  value={name}
-                  onChange={onNameChange}
-                  onKeyDown={(e) => e.stopPropagation()}
-                  style={{ width: "70%" }}
-                />
-                <Button
-                  type="text"
-                  size="large"
-                  style={{
-                    backgroundColor: "black",
-                    margin: "0 10px",
-                    color: "#fff",
-                  }}
-                  icon={<PlusOutlined twoToneColor="#fff" />}
-                  onClick={addItem}
-                >
-                  Add item
-                </Button>
-              </div>
-            </>
-          )}
-          options={options}
-          onChange={onChange}
-          value={value}
-        />
+      disabled={disabled}
+      size="large"
+      placeholder={placeholder ? placeholder : ""}
+      dropdownRender={(menu) => (
+        <>
+          {menu}
+          <Divider style={{ margin: "8px 0" }} />
+          <div
+            style={{ padding: "0 8px 4px", width: "100%" }}
+            className="d-flex"
+          >
+            <Input
+              placeholder="Please enter item"
+              value={name}
+              onChange={onNameChange}
+              onKeyDown={(e) => e.stopPropagation()}
+              style={{ width: "70%" }}
+            />
+            <Button
+              type="text"
+              size="large"
+              style={{
+                backgroundColor: "black",
+                margin: "0 10px",
+                color: "#fff",
+              }}
+              icon={<PlusOutlined twoToneColor="#fff" />}
+              onClick={addItem}
+            >
+              Add item
+            </Button>
+          </div>
+        </>
+      )}
+      options={options}
+      onChange={onChange}
+      value={value}
+    />
   );
 };
 export default CustomSelect;
