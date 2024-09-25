a333134b95a58c19ddd9749a9f1736bf45599d47
abinaya-innoura <abinaya@innoura.com>
2024-09-25 18:13:03 +0530

    remove console

--- src/jsx/layouts/nav/Header.js
+++ src/jsx/layouts/nav/Header.js
@@ -297,7 +297,7 @@ const Header = ({
     localStorage.setItem("userRole", key);
     if (key === "admin") {
       router.push("/admin/dashboard");
-    } else if (key === "reviewer") {git 
+    } else if (key === "reviewer") {
       router.push("/reviewer/dashboard");
     } else if (key === "supervisor") {
       router.push("/supervisor/dashboard");
