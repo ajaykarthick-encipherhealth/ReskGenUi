6113b225d87dbc20a1d1272e6d288b7a463d156a
ambika-encipher <ambika@encipherhealth.com>
2024-09-25 18:09:05 +0530

    import getStorage

--- src/jsx/layouts/nav/Header.js
+++ src/jsx/layouts/nav/Header.js
@@ -59,6 +59,7 @@ import {
 import { actions as dashbaordActions } from "../../../stores/reviewer/dashboard";
 import Codify from "../../../pages/codify";
 import { actions as webSocketActions } from "../../../stores/websocket";
+import { getStorage } from "../../../utils/storages";
 
 const Header = ({
   notificationResponse,
