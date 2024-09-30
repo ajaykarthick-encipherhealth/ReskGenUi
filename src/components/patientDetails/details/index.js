1b521cd84c6fe0bed55434bd626e2f73d4fcbabe
priya-encipherhealth <priya@encipherhealth.com>
2024-09-30 15:21:08 +0530

    filename issue fixed

--- src/components/patientDetails/details/index.js
+++ src/components/patientDetails/details/index.js
@@ -56,6 +56,7 @@ import { PlusCircleOutlined } from "@ant-design/icons";
 import ManuallyAddProvider from "./manuallyAddProvider";
 import { getAge } from "../../../utils/reusable";
 import { getStorage } from "../../../utils/storages";
+import { truncateString } from "./components/function/ReusableFunctions";
 
 export const navigetPageDetails = async (
   pageTitle,
@@ -770,12 +771,34 @@ const Details = ({
                                     <FontAwesomeIcon icon={faFile} />
 
                                     <label>File Name</label>
-                                    <h6 className="ageDtails">
+                                    <div
+                                      style={{
+                                        width: "80px",
+                                        textOverflow: "ellipsis",
+                                      }}
+                                      className="text-truncate"
+                                    >
+                                      {/* <h6 >
                                       {
                                         patientDocumentResult?.fileDetailDTO
                                           ?.fileName
                                       }
-                                    </h6>
+                                    </h6> */}
+                                      <h6>
+                                        <Tooltip
+                                          title={
+                                            patientDocumentResult?.fileDetailDTO
+                                              ?.fileName
+                                          }
+                                        >
+                                          {truncateString(
+                                            patientDocumentResult?.fileDetailDTO
+                                              ?.fileName,
+                                            8
+                                          )}
+                                        </Tooltip>
+                                      </h6>
+                                    </div>
                                   </div>
                                   <div className="col-xl-1 col-sm-12">
                                     <FontAwesomeIcon icon={faCalendarAlt} />
@@ -822,7 +845,12 @@ const Details = ({
                                   <div className="col-xl-1 col-sm-12">
                                     {flagsDetailsResult?.response[0] && (
                                       <div className="mt-2">
-                                        <div className="d-flex align-items-center justify-content-center cr-pointer" onClick={() => setFlagContainerActive('Flag')}>
+                                        <div
+                                          className="d-flex align-items-center justify-content-center cr-pointer"
+                                          onClick={() =>
+                                            setFlagContainerActive("Flag")
+                                          }
+                                        >
                                           <Tooltip
                                             placement="bottom"
                                             title={flagsDetailsResult?.response[0]?.flagDetails?.flagName.replaceAll(
