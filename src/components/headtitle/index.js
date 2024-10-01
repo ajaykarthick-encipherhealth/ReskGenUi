3834b8b9f2ed409eb32447cf93a4c206ba0baa83
priya-encipherhealth <priya@encipherhealth.com>
2024-10-01 19:19:45 +0530

    removed empty space

--- src/components/headtitle/index.js
+++ src/components/headtitle/index.js
@@ -81,7 +81,6 @@ const HeadTitle = ({
           View All
         </span>
       )}
-
         <Modal
           open={openPicker}
           mask={true}
@@ -119,7 +118,6 @@ const HeadTitle = ({
           </div>
           <div id="date-popup" style={{ position: "relative" }} />
         </Modal>
-
     </div>
   );
 };
