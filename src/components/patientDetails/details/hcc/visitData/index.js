533792e9fe4242bedb4835c3681d1c86e36df991
abinaya-innoura <abinaya@innoura.com>
2024-10-03 18:38:37 +0530

    click also caregap visitdata modal

--- src/components/patientDetails/details/hcc/visitData/index.js
+++ src/components/patientDetails/details/hcc/visitData/index.js
@@ -337,13 +337,22 @@ const VisitData = ({
                         {...provided.droppableProps}
                         ref={provided.innerRef}
                       >
-                        <div
+                        <div  onClick={() => handleShowList("care")}
                           className={`valid-text d-flex justify-content-sm-between ${visitStyles.suggested_title_card}`}
                         >
                           <span
                             className={`${visitStyles.suggested_title_name}`}
                           >
-                            CARE GAP
+                             <span className="mx-1">
+                            <FontAwesomeIcon
+                              icon={
+                                showList.includes("care")
+                                  ? faAngleDown
+                                  : faAngleRight
+                              }
+                            />
+                          </span>
+                            CARE GAP  / SUGGESTED CODES
                           </span>
                           <div className="d-flex justify-content-center">
                             <span
