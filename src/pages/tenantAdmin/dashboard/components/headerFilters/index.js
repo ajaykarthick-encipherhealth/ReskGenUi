4756f68f5c7dd77ae256915f6b2c53b443a00481
abinaya-encipherhealth <abinya@encipherhealth.com>
2024-10-14 17:41:16 +0530

    change width in picker

--- src/pages/tenantAdmin/dashboard/components/headerFilters/index.js
+++ src/pages/tenantAdmin/dashboard/components/headerFilters/index.js
@@ -83,7 +83,7 @@ const index = ({
   return (
     <div className={styles.container}>
       <div className="row" style={{ width: "80%" }}>
-        <div className="d-flex"  style={{ width: "30%" }}>
+        <div className="d-flex"  style={{ width: "25%" }}>
           <div className={styles.flterContainer}>Organization</div>
           <div className="tenantSelector" style={{ width: "100%" }}>
             <Select
