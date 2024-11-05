88801af32ef2032a16ad6cd02b3baf366b9d966c
ambika-encipher <147126498+ambika-encipher@users.noreply.github.com>
2024-11-05 18:31:00 +0530

    resolved build error (#1938)

--- src/pages/supervisor/auditing/filters/index.js
+++ src/pages/supervisor/auditing/filters/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { DatePicker, Popover, Select, Tooltip } from "antd";
+import { Popover, Select, Tooltip } from "antd";
 import Image from "next/image";
 import styles from "../../../../pages/reviewer/report/report.module.css";
 import Legends from "../../../../components/legends";
@@ -8,13 +8,10 @@ import Search from "../../../../components/search";
 import { resetPageNumber } from "../../../../components/headerFilters/functions";
 import { InfoCircleFilled } from "@ant-design/icons";
 import MoreFilter from "../../../tenantAdmin/tracking/filters";
-
-const { RangePicker } = DatePicker;
 const allFilters = ["Reviewer Status", "Select Audited Status", "Audited Date"];
 
 const Filters = ({
   setSearch,
-  isSearch,
   searchlabel,
   coderSearch,
   receivedSearch,
@@ -22,36 +19,17 @@ const Filters = ({
   search,
   searchVal,
   setSearchVal,
-
   // Report Props
   setSentSearch,
   setReceivedSearch,
   setCoderSearch,
-
   // Select Props
   selectlabel,
-  isSelector,
   setSelectedOption,
   selectOptions,
   selectedOption,
-
-  defaultSelectValue1,
-  selectlabel2,
-  defaultSelectValue2,
-  selectOptions2,
-  setSelectedOption2,
-  selectlabel3,
-  selectOptions3,
-  isSelector3,
-  setSelectedOption3,
-
-  // Picker Props
-  pickerlabel,
   pickerlabe2,
-  pickerlabe4,
-  pickerlabe5,
   activeTab,
-  selectedDates,
   setSelectedDates,
   selectedDates2,
   setSelectedDates2,
@@ -59,116 +37,23 @@ const Filters = ({
   defaultEndDate,
   setStartDate,
   setEndDate,
-  setReceivedStartDate,
-  setReceivedEndDate,
-  setCoderStartDate,
-  setCoderEndDate,
   setStartDate2,
   setEndDate2,
-  defaultStartDate2,
-  defaultEndDate2,
-  //if has time picker
-  isRangeTimePicker,
-  timePickerlabel,
-  defaultStartTime,
-  defaultEndTime,
-  setStartTime,
-  setEndTime,
-  setSelectedTime,
-  setReceivedStartTime,
-  setReceivedEndTime,
-  setCoderStartTime,
-  setCoderEndTime,
-
-  // if has allocated date picker
-  pickerlabe3,
-  defaultStartDate3,
-  defaultEndDate3,
-  setStartDate3,
-  setEndDate3,
-  setStartDate4,
-  setEndDate4,
-  setStartDate5,
-  setEndDate5,
-  isRangePicker,
-  isAnotherPicker,
-  isAnotherPicker2,
-  isAnotherPicker3,
-  isAnotherPicker5,
-
-  // Additional Props
   addUser,
-  rowsLength,
-  addUserForm,
-  selectedRowsId,
-  handleOpneModal,
-  isAllocate,
-  isAllocatedBySelector,
-  allocatedBylabel,
-  allocatedByOptoons,
-  setSelAllocatedBy,
-  defaultAllocatedBy,
-  isAllocatedToSelector,
-  allocatedTolabel,
-  allocatedToOptoons,
-  setSelAllocatedTo,
-  defaultAllocateTo,
-
-  //priority
-  isAnotherPicker6,
-  pickerlabe6,
-  setPriority,
-  defaultPriority,
-
-  isCreatedBySelector,
   createdTolabel,
   createdByOptoons,
   setSelCreatedBy,
-  defaultCreatedBy,
-  selectedCoderOptReport,
   bullets,
-  isNextRow,
-  btnTitle,
   badges,
-  setIsModalVisible,
-  optionKey,
-  disable,
-  tracking,
-  selectorField,
-  defaultShow = false,
-  setSelect,
-  defaultSize = "col-xl-2 col-md-3",
-  adminReport,
   addBtn,
-  atCorner,
-  isNextCreatedBySelector,
-  selectReportOptions,
-  value,
-  setSelectedManger,
-
-  // selectOrg
-  selectlabelOrg,
   isSelectOrg,
-  setSelectedOptionOrg,
-  selectOptionsOrg,
-  defaultSelectValueOrg,
-  selectedValueOrg,
-  isRangePickerUsers,
-  form,
-  setMobileNumber,
   setPageNo,
-  selectDefaultValue,
-  orgValue,
-  fromTenantPatients,
-  selAllocatedBy,
-  clear,
   selCreatedBy,
   setClear,
-  activeFilters,setActiveFilters
+  activeFilters,
+  setActiveFilters,
 }) => {
   const [selectAll, setSelectAll] = useState(false);
-  const [popoverVisible, setPopoverVisible] = useState(false);
-
   const handleClearAllFilters = () => {
     setClear(true);
     setStartDate([]);
@@ -177,7 +62,6 @@ const Filters = ({
     setSelectedDates([]);
     setSelectedDates2([]);
     setSelectedOption();
-    setPopoverVisible(false);
     setSelCreatedBy("");
   };
   let columnClass;
@@ -282,7 +166,7 @@ const Filters = ({
             setPageNo={setPageNo}
           />
         </div>
-        {activeFilters.map((filter) => (
+        {activeFilters?.map((filter) => (
           <React.Fragment key={filter}>{renderFilter(filter)}</React.Fragment>
         ))}
       </div>
