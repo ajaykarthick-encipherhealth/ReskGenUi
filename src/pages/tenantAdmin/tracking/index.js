b4a55da59716bdbc557ec0fec41d8cea482aaec2
ambika-encipher <147126498+ambika-encipher@users.noreply.github.com>
2024-12-12 13:08:09 +0530

    commented-aborted-by-cron-option (#2023)

--- src/pages/tenantAdmin/tracking/index.js
+++ src/pages/tenantAdmin/tracking/index.js
@@ -90,7 +90,7 @@ const statusOptions = [
   { label: "PENDING", value: "PENDING", status: 0 },
   { label: "DECLINED", value: "DECLINED", status: 0 },
   { label: "HOLD", value: "HOLD", status: 0 },
-  { label: "ABORTED BY CRON", value: "ABORTED_BY_CRON" },
+  // { label: "ABORTED BY CRON", value: "ABORTED_BY_CRON" },
 ];
 
 const auditStatusOptions = [
