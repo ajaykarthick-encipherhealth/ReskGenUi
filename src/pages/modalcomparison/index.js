1e531f3c9f4828380f506083e12fd0cd375de279
vignesh-encipher <151368623+vignesh-encipher@users.noreply.github.com>
2025-02-05 13:46:26 +0530

    new changes (#2249)
    Co-authored-by: vignesh-innoura <vignesh@innoura.com>

--- src/pages/modalcomparison/index.js
+++ src/pages/modalcomparison/index.js
@@ -1,33 +1,53 @@
 import { useState } from "react";
 import { connect } from "react-redux";
 import { actions as allActions } from "../../stores/chatService";
-import { ProgressSpinner } from "primereact/progressspinner"; // Import PrimeReact Spinner
+import { ProgressSpinner } from "primereact/progressspinner";
+import CardSkeleton from "../../components/skeleton/card";
 
 const ModalComparison = ({ getChatReply, getEHReply }) => {
-  const [openAiText, setOpenAiText] = useState("");
-  const [ehText, setEhText] = useState("");
+  const [patientRecord, setPatientRecord] = useState("");
   const [openAiResponse, setOpenAiResponse] = useState("");
   const [ehResponse, setEhResponse] = useState("");
-  const [showResponses, setShowResponses] = useState(false);
-  const [loading, setLoading] = useState(false); // Loading state
+  const [chatLoading, setChatLoading] = useState(false);
+  const [aiLoading, setAiLoading] = useState(false);
+  const [initialHit, setInitialHit] = useState(false)
 
   const handleProceed = async () => {
     try {
-      setLoading(true); // Start loading
-
-      const openAiData = await getChatReply(openAiText);
-      setOpenAiResponse(openAiData.response || "No response received");
-
-      const ehData = await getEHReply(ehText);
-      setEhResponse(ehData.response || "No response received");
-
-      setShowResponses(true);
+      setChatLoading(true);
+      const openAiData = await getChatReply(patientRecord);
+      if (ehResponse) {
+        setTimeout(() => {
+          setOpenAiResponse(openAiData.response || "No response received");
+        }, 3000);
+      } else {
+        setTimeout(() => {
+          setOpenAiResponse(openAiData.response || "No response received");
+        }, 3000);
+      }
     } catch (error) {
-      console.error("Error fetching responses:", error);
       setOpenAiResponse("Error fetching response");
+    } finally {
+      setChatLoading(false);
+    }
+  };
+
+  const handleProceeds = async () => {
+    try {
+      setAiLoading(true);
+      const ehData = await getEHReply(patientRecord);
+      if (ehData.response.model == "code") {
+        setTimeout(() => {
+          setEhResponse(ehData.response.response || "No response received");
+          console.log(ehData.response.model);
+        }, 6000);
+      } else {
+        setEhResponse(ehData.response.response || "No response received");
+      }
+    } catch (error) {
       setEhResponse("Error fetching response");
     } finally {
-      setLoading(false); // Stop loading
+      setAiLoading(false);
     }
   };
 
@@ -39,129 +59,109 @@ const ModalComparison = ({ getChatReply, getEHReply }) => {
         alignItems: "center",
         gap: "30px",
         padding: "20px",
-        background: "#f4f4f4",
+        background: "rgb(244, 244, 244)",
         borderRadius: "10px",
         boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)",
-        maxWidth: "800px",
+        maxWidth: "2000px",
         margin: "auto",
         marginTop: "50px",
         fontFamily: "Arial, sans-serif",
+        height: "100vh",
       }}
     >
-      <h2 style={{ color: "#333" }}>Model Comparison</h2>
+      <h2 style={{ color: "#333" }}>Patient Record Comparison</h2>
 
-      {/* Input Fields */}
-      <div style={{ display: "flex", gap: "30px", width: "90%" }}>
-        <div style={{ display: "flex", flexDirection: "column", width: "50%" }}>
-          <label style={{ fontWeight: "bold", marginBottom: "5px" }}>
-            OpenAI Model
-          </label>
-          <textarea
-            rows={10}
-            style={{
-              border: "1px solid #ccc",
-              padding: "12px",
-              width: "100%",
-              borderRadius: "8px",
-              minHeight: "120px",
-              fontSize: "14px",
-              fontFamily: "Arial, sans-serif",
-            }}
-            value={openAiText}
-            onChange={(e) => setOpenAiText(e.target.value)}
-          />
-        </div>
-        <div style={{ display: "flex", flexDirection: "column", width: "50%" }}>
-          <label style={{ fontWeight: "bold", marginBottom: "5px" }}>
-            EH Model
-          </label>
-          <textarea
-            rows={10}
-            style={{
-              border: "1px solid #ccc",
-              padding: "12px",
-              width: "100%",
-              borderRadius: "8px",
-              minHeight: "120px",
-              fontSize: "14px",
-              fontFamily: "Arial, sans-serif",
-            }}
-            value={ehText}
-            onChange={(e) => setEhText(e.target.value)}
-          />
-        </div>
+      <div style={{ width: "90%" }}>
+        <label style={{ fontWeight: "bold", marginBottom: "5px" }}>
+          Patient Record
+        </label>
+        <textarea
+          placeholder="Enter patient record..."
+          rows={10}
+          style={{
+            border: "1px solid #ccc",
+            padding: "12px",
+            width: "100%",
+            borderRadius: "8px",
+            fontSize: "14px",
+            fontFamily: "Arial, sans-serif",
+          }}
+          value={patientRecord}
+          onChange={(e) => setPatientRecord(e.target.value)}
+        />
       </div>
 
-      {/* Process Button with Loading Indicator */}
       <button
-        onClick={handleProceed}
+        onClick={() => {
+          setEhResponse("");
+          setOpenAiResponse("");
+          handleProceed();
+          handleProceeds();
+          setInitialHit(true)
+        }}
         style={{
           padding: "12px 20px",
           border: "none",
-          background: loading ? "#ccc" : "#007bff",
+          background: "#007bff",
           color: "white",
-          cursor: loading ? "not-allowed" : "pointer",
+          cursor: "pointer",
           borderRadius: "5px",
           fontSize: "16px",
           fontWeight: "bold",
         }}
-        disabled={loading} // Disable button while loading
+        // disabled={chatLoading}
       >
-        {loading ? "Processing..." : "Process"}
+        {"Process"}
       </button>
-
-      {/* Loading Spinner */}
-      {loading && <ProgressSpinner />}
-
-      {/* Responses */}
-      {showResponses && !loading && (
+      {initialHit && 
         <div style={{ display: "flex", gap: "30px", width: "90%" }}>
-          <div
-            style={{ display: "flex", flexDirection: "column", width: "50%" }}
-          >
+          <div style={{ flex: 1 }}>
             <label style={{ fontWeight: "bold", marginBottom: "5px" }}>
-              OpenAI Response
+              Open Source AI Response
             </label>
-            <textarea
-              rows={10}
-              style={{
-                border: "1px solid #ccc",
-                padding: "12px",
-                width: "100%",
-                borderRadius: "8px",
-                minHeight: "120px",
-                background: "#e9ecef",
-                fontSize: "14px",
-                fontFamily: "Arial, sans-serif",
-              }}
-              value={openAiResponse}
-              readOnly
-            />
+            {chatLoading || !openAiResponse ? (
+              <CardSkeleton height={340} />
+            ) : (
+              <textarea
+                rows={15}
+                style={{
+                  border: "1px solid #ccc",
+                  padding: "12px",
+                  width: "100%",
+                  borderRadius: "8px",
+                  background: "#e9ecef",
+                  fontSize: "14px",
+                  fontFamily: "Arial, sans-serif",
+                }}
+                value={openAiResponse}
+                readOnly
+              />
+            )}
           </div>
-          <div
-            style={{ display: "flex", flexDirection: "column", width: "50%" }}
-          >
+          <div style={{ flex: 1 }}>
             <label style={{ fontWeight: "bold", marginBottom: "5px" }}>
-              EH Response
+              EH AI Response
             </label>
-            <textarea
-              rows={10}
-              style={{
-                border: "1px solid #ccc",
-                padding: "12px",
-                width: "100%",
-                borderRadius: "8px",
-                minHeight: "120px",
-                background: "#e9ecef",
-                fontSize: "14px",
-                fontFamily: "Arial, sans-serif",
-              }}
-              value={ehResponse}
-              readOnly
-            />
+            {aiLoading || !ehResponse ? (
+              <CardSkeleton height={340} />
+            ) : (
+              <textarea
+                rows={15}
+                style={{
+                  border: "1px solid #ccc",
+                  padding: "12px",
+                  width: "100%",
+                  borderRadius: "8px",
+                  background: "#e9ecef",
+                  fontSize: "14px",
+                  fontFamily: "Arial, sans-serif",
+                }}
+                value={ehResponse}
+                readOnly
+              />
+            )}
           </div>
-        </div>
-      )}
+        </div> }
     </div>
   );
 };
