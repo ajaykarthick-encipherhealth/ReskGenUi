04730bd2de4a479e642cf3fd45ab6ad37fcae565
priya-encipherhealth <141149107+priya-encipherhealth@users.noreply.github.com>
2025-03-10 10:25:49 +0530

    speedometer issue fixed (#2385)
    * speedometer issue fixed
    
    * replaced name

--- src/pages/tenantadmin/dashboard/default/rafAndRevenue/index.js
+++ src/pages/tenantadmin/dashboard/default/rafAndRevenue/index.js
@@ -1,5 +1,4 @@
-import React from "react";
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 import { rafScore } from "../../../../../stores/tenantAdmin/dashboard/default/action.js";
 import { connect } from "react-redux";
 import ReactECharts from "echarts-for-react";
@@ -7,6 +6,10 @@ import styles from "../../styles.module.css";
 import { Skeleton } from "antd";
 import CodeGraphRevenue from "../../components/codeGraphRevenue/index.js";
 import { formatNumber } from "../../../../../utils/reusable.js";
+
+
+const getMaxValue = (value) =>
+  value > 0 ? Math.ceil(value / 1000) * 1000 : 1000;
 
 const index = ({
   rafScoreData,
@@ -19,11 +22,9 @@ const index = ({
   customDate,
   selectDos,
 }) => {
-  // Function to round up to the nearest 1000
-  const getMaxValue = (value) => Math.ceil(value / 1000) * 1000;
-
   const responseValue = parseFloat(overAllRafScore?.response ?? 0);
   const maxValue = getMaxValue(responseValue);
+
 
   const speedometerOptions = {
     tooltip: {
@@ -36,7 +37,7 @@ const index = ({
         min: 0,
         max: maxValue,
         progress: {
-          show: true,
+          show: responseValue > 0, 
           roundCap: true,
           width: 5,
           itemStyle: {
@@ -56,7 +57,7 @@ const index = ({
           offsetCenter: [0, "10%"],
         },
         pointer: {
-          show: false,
+          show: false
         },
         axisLine: {
           roundCap: true,
@@ -83,7 +84,7 @@ const index = ({
         },
         data: [
           {
-            value: parseFloat(overAllRafScore?.response?.toFixed(2)),
+            value: responseValue > 0 ? responseValue : 0, 
             name: "",
           },
         ],
@@ -128,14 +129,12 @@ const index = ({
           <div className={styles.price}>{`$ ${
             formatNumber(totalRev) || "0.00"
           }`}</div>
-          {/* <div className={styles.revenue}>$ 3.1k Increase</div> */}
         </div>
         <CodeGraphRevenue
           gradientColor1={"#5D94FE"}
           gradientColor2={"#FAFCFF"}
           borderColor={"#3479FE"}
           isRevenue={true}
-          className
           customDate={customDate}
           selectedValue={selectedValue}
         />
@@ -151,7 +150,6 @@ const enhancer = connect(
     overAllRafScore: state?.tenantAdmin?.dashboard?.default?.allRafScore?.data,
     rafLoader: state?.tenantAdmin?.dashboard?.default?.rafScoreLoader,
   }),
-
   {
     rafScoreData: rafScore,
   }
