465b78540e751806bac2f64da10af76a9775f9c6
ajith-encipherhealth <145423565+ajith-encipherhealth@users.noreply.github.com>
2025-04-16 20:11:16 +0530

    api changes pushed (#2486)
    * api changes pushed
    
    * code formatted

--- src/components/patientDetails/details/components/yearAndDosStatus/index.js
+++ src/components/patientDetails/details/components/yearAndDosStatus/index.js
@@ -36,6 +36,7 @@ const YearAndDosStatus = ({
   const [inputValue, setInputValue] = useState({
     notes: "",
   });
+  const [statusName, setStatusName] = useState('');
 
   const renderAuditMenu = (value) => {
     var value = (
@@ -374,6 +375,7 @@ const YearAndDosStatus = ({
     setActionItems3(menu3);
   };
   const handleActionClick = (value) => {
+    setStatusName(value);
     if (value == "HOLD") {
       setConfirmNotesModal(true);
       setIsValidAction("holdFunction");
@@ -427,6 +429,8 @@ const YearAndDosStatus = ({
       notes: inputValue.notes,
       processedYear: patientDetailsResult?.data?.response?.processedYear,
       dateOfService: patientDetailsResult?.data?.response?.dateOfService,
+      roleId:getStorage("userRoleId"),
+      processedStatus:statusName
     };
     var apiURL = "";
     if (action == "declineFunction") {
