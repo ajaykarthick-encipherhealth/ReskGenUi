4ccd74b7c1d90495a97b420807a8c47ebfea707a
abinaya-encipherhealth <abinya@encipherhealth.com>
2025-05-22 18:33:18 +0530

    progress bar width change

--- src/components/tables/index.js
+++ src/components/tables/index.js
@@ -1145,7 +1145,7 @@ const TableRow = ({
                   //     : "#263E50"
                   // }
                   format={(percent) => `${percent}%`} 
-                  className={`w-100 ${Style.progreddBr}`}
+                  className={`w-75 ${Style.progreddBr}`}
                 />
               </div>
             </td>
