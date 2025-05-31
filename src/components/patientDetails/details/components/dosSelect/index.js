c5ceff3db4dc1d85c3dd3055b639a14bb7bcde5d
priya-encipherhealth <141149107+priya-encipherhealth@users.noreply.github.com>
2025-05-31 12:35:44 +0530

    details changes (#2728)

--- src/components/patientDetails/details/components/dosSelect/index.js
+++ src/components/patientDetails/details/components/dosSelect/index.js
@@ -136,7 +136,7 @@ const DosSelect = ({
       open={open}
       onDropdownVisibleChange={(visible) => setOpen(visible)}
       dropdownRender={() => (
-        <div style={{ padding: 0, maxHeight: "200px", overflowY: "scroll" }}>
+        <div style={{ padding: 0, maxHeight: "100px", overflowY: "scroll" }}>
           {/* Header row */}
           <div
             style={{
