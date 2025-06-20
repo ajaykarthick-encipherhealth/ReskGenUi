1ec637ead47c9a0fa770d64b288cbaead9ad5fe3
Abinaya M <159226489+abinaya-encipherhealth@users.noreply.github.com>
2025-06-20 12:56:19 +0530

    role switching issue fixed (#2816)
    Co-authored-by: abinaya-encipherhealth <abinya@encipherhealth.com>

--- src/jsx/layouts/nav/Header.js
+++ src/jsx/layouts/nav/Header.js
@@ -225,14 +225,13 @@ const Header = ({
     const defaultProject = getStorage("project");
     const defaultTinNumber = getStorage("tinNumber");
     const userRole = getStorage("proxyRole");
-    if (tinDetails?.length > 0 && userRole === "QA") {
+    if (tinDetails?.length > 0 && 
+      key === "QA") {
       let initialTin = defaultTinNumber || tinDetails[0].tinNumber;
-      if (!selectedTin) {
         setSelectedTin(initialTin);
         setStorage("tinNumber", initialTin);
         setBackupSelectedTin(initialTin);
       }
-    }
     if (defaultClient) {
       setSelectedClient(defaultClient);
       setBackupSelectedClient(defaultClient);
