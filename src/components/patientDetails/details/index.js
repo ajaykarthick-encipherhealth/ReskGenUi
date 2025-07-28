290766b01d26dadd0656668fc6af0d4ce46819c4
Abinaya M <159226489+abinaya-encipherhealth@users.noreply.github.com>
2025-07-28 12:54:40 +0530

    timeline value clear bug fixed (#2995)

--- src/components/patientDetails/details/index.js
+++ src/components/patientDetails/details/index.js
@@ -508,6 +508,8 @@ const Details = ({
     setFlagContainerActive("");
     setShowFilter(false);
     setIsViewAll(false);
+    setRole([]);
+    setAction([]);
   };
 
   const dosOnChange = async (e) => {
