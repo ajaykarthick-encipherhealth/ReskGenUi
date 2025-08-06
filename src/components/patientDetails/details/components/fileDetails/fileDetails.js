9b7d64123ca003edbfbb470c2b1d76804ff37b8e
vignesh-encipher <151368623+vignesh-encipher@users.noreply.github.com>
2025-08-06 13:36:15 +0530

    new design changes for detiles change for header (#3078)
    * new design changes for detiles change for header
    
    * respociveness
    
    * responiveness
    
    ---------
    
    Co-authored-by: vignesh-innoura <vignesh@innoura.com>

--- src/components/patientDetails/details/components/fileDetails/fileDetails.js
+++ src/components/patientDetails/details/components/fileDetails/fileDetails.js
@@ -19,8 +19,10 @@ import { notification, Tooltip } from "antd";
 import { truncateString } from "../function/ReusableFunctions";
 import { handleCopyToClipboard } from "../../../../commonFunctions";
 import { formatDateTime } from "../../../../../utils/reusable";
+import { useWindowWidth } from "../../../../../commonPages/dashboard/component/function";
 
 const Details = ({ fileResult, fromHcc, fileDetails }) => {
+  const size = useWindowWidth()
   const getMastData = (value) => {
     if (value) {
       return value.split("").splice(0, 3).join("") + "xxxx";
@@ -32,202 +34,128 @@ const Details = ({ fileResult, fromHcc, fileDetails }) => {
       return dayjs().diff(dob, "year");
     }
   };
+  const getHeaderData = [
+    {
+      title: "Patient ID",
+      value: fileResult?.patientId || "---",
+      icon: faIdCardClip,
+      isMask: true,
+      isCopyed: true,
+      isSvg: false,
+    },
+    {
+      title: "Patient Name",
+      value: fileResult?.patientName || "---",
+      icon: faUserCircle,
+      isMask: true,
+      isCopyed: true,
+      isSvg: false,
+    },
+    {
+      title: "MBI",
+      value: fileResult?.mbi || "---",
+      icon: faIdCard,
+      isMask: true,
+      isCopyed: true,
+      isSvg: false,
+    },
+    {
+      title: "Age",
+      value: calculateAge(fileResult?.dob) || "---",
+      icon: faCalendarAlt,
+      isMask: false,
+      isCopyed: false,
+      isSvg: false,
+    },
+    {
+      title: "File Name",
+      value: fileResult?.fileName || "---",
+      icon: faFile,
+      isMask: true,
+      isCopyed: true,
+      isSvg: false,
+    },
+    {
+      title: "Date Of Birth",
+      value: fileResult?.dob
+        ? formatDateTime({ date: fileResult.dob })?.replace(/\d{4}$/, "xxxx")
+        : "---",
+      icon: <i className={styles.dob_icon}>{SVGICON.DatebirthIcon}</i>,
+      isMask: false,
+      isCopyed: true,
+      isSvg: true,
+    },
+    {
+      title: "Gender",
+      value: fileResult?.gender || "---",
+      icon: faVenusMars,
+      isMask: false,
+      isCopyed: false,
+      isSvg: false,
+    },
+    {
+      title: "Face to Face",
+      value: fileDetails?.faceToFace ? "Yes" : "No",
+      icon: SVGICON.faceToface,
+      isMask: false,
+      isCopyed: false,
+      isSvg: true,
+    },
+    {
+      title: "Visit Type",
+      value: fileDetails?.visitType || "---",
+      icon: SVGICON.visitType,
+      isMask: false,
+      isCopyed: false,
+      isSvg: true,
+    },
+  ];
+  
   return (
     <>
       <div
         id="detailsCardHcc"
         name="detailsCardHcc"
         className={styles.detailsCardHcc}
-      >
-        <div
-          id="patient-header"
-          name="patient-header"
-          className="row"
-          style={{ lineHeight: "0" }}
-        >
-          <div className="col-2" id="patient-id" name="patient-id">
-            <FontAwesomeIcon icon={faIdCardClip} style={{ color: "#241571" }} />
-            <label className="px-1" style={{ fontWeight: 600 }}>
-              Patient ID
-            </label>
-            <h6
-              id="patient-id-value"
-              name="patient-id-value"
-              className="px-4 cursor-pointer"
-              onClick={() =>
-                handleCopyToClipboard({
-                  text: fileResult?.patientId,
-                  setCopied: setCopied,
-                })
-              }
-            >
-              <Tooltip title={fileResult?.patientId}>
-                {fileResult?.patientId
-                  ? getMastData(fileResult?.patientId)
-                  : "-"}
-              </Tooltip>
-            </h6>
-          </div>
-          <div className="col-2" id="patient-name" name="patient-name">
-            <FontAwesomeIcon icon={faUserCircle} style={{ color: "#241571" }} />
-
-            <label className="px-2" style={{ fontWeight: 600 }}>
-              Patient Name
-            </label>
-            <h6
-              id="patient-name-value"
-              name="patient-name-value"
-              className="px-4 cursor-pointer"
-              onClick={() =>
-                handleCopyToClipboard({
-                  text: fileResult?.patientName,
-                  setCopied: setCopied,
-                })
-              }
-            >
-              <Tooltip title={fileResult?.patientName}>
-                {fileResult?.patientName
-                  ? getMastData(fileResult?.patientName)
-                  : "-"}
-              </Tooltip>
-            </h6>
-          </div>
-          <div className="col-2" id="patient-mbi" name="patient-mbi">
-            <FontAwesomeIcon icon={faIdCard} style={{ color: "#241571" }} />
-
-            <label className={styles.mbi} style={{ fontWeight: 600 }}>
-              MBI
-            </label>
-            <h6
-              id="patient-mbi-value"
-              name="patient-mbi-value"
-              className="px-4 cursor-pointer"
-              onClick={() =>
-                handleCopyToClipboard({
-                  text: fileResult?.mbi,
-                  setCopied: setCopied,
-                })
-              }
-            >
-              <Tooltip title={fileResult?.mbi}>
-                {fileResult?.mbi ? getMastData(fileResult?.mbi) : "-"}
-              </Tooltip>
-            </h6>
-          </div>
-          <div className="col-2" id="patient-age" name="patient-age">
-            <FontAwesomeIcon
-              icon={faCalendarAlt}
-              style={{ color: "#241571" }}
-            />
-            <label className="px-2" style={{ fontWeight: 600 }}>
-              Age
-            </label>
-            <h6
-              id="patient-age-value"
-              name="patient-age-value"
-              className="px-4"
-            >
-              {fileResult?.dob ? calculateAge(fileResult?.dob) : "-"}
-            </h6>
-          </div>
-          <div className="col-2" id="file-name" name="file-name">
-            <FontAwesomeIcon icon={faFile} style={{ color: "#241571" }} />
-            <label className="px-1" style={{ fontWeight: 600 }}>
-              File Name
-            </label>
-            <h6
-              id="file-name-value"
-              name="file-name-value"
-              // className="px-2"
-              style={{
-                paddingLeft: "25px",
-                cursor: "pointer",
-              }}
-              onClick={() =>
-                handleCopyToClipboard({
-                  text: fileResult?.fileName,
-                  setCopied: setCopied,
-                })
-              }
-            >
-              {fileResult?.fileName ? (
-                <Tooltip title={fileResult?.fileName}>
-                  {getMastData(fileResult?.fileName)}
-                </Tooltip>
+      > 
+        <div className={size <= 1130 ? "row" : "d-flex gap-3 justify-content-between"}>
+          {getHeaderData.map((item) => (
+            <div className={size <= 1130 ? "col-2" : ""} id="patient-id" name="patient-id">
+              {item.isSvg ? (
+                item.icon
               ) : (
-                "-"
+                <FontAwesomeIcon
+                  icon={item.icon}
+                  style={{ color: "#241571" }}
+                />
               )}
-            </h6>
-          </div>
-          <div className="col-2 " id="date-of-birth" name="date-of-birth">
-            <i className={styles.dob_icon}>{SVGICON.DatebirthIcon}</i>
-            <label className="px-2" style={{ fontWeight: 600 }}>
-              Date Of Birth
-            </label>
-            <h6
-              id="date-of-birth-value"
-              name="date-of-birth-value"
-              className="px-4 cursor-pointer"
-              onClick={() =>
-                handleCopyToClipboard({
-                  text: fileResult?.dob,
-                  setCopied: setCopied,
-                })
-              }
-            >
-              <Tooltip
-                title={formatDateTime({
-                  date: fileResult?.dob,
-                })}
+              <label className="px-1 m-0" style={{ fontWeight: 600 }}>
+                {item.title}
+              </label>
+              <h6
+                id={item.title.replaceAll(" ", "-").toLowerCase()}
+                name={item.title.replaceAll(" ", "-").toLowerCase()}
+                className="px-4 cursor-pointer"
+                onClick={() =>
+                  item.isCopyed &&
+                  handleCopyToClipboard({
+                    text: item.value,
+                    setCopied: setCopied,
+                  })
+                }
               >
-                {fileResult?.dob
-                  ? formatDateTime({ date: fileResult.dob })?.replace(
-                      /\d{4}$/,
-                      "xxxx"
-                    )
-                  : "-"}
-              </Tooltip>
-            </h6>
-          </div>
-          <div className="col-2" id="gender" name="gender">
-            <FontAwesomeIcon icon={faVenusMars} style={{ color: "#241571" }} />
-            <div className={styles.subItems}>
-              <label className="px-3" style={{ fontWeight: 600 }}>
-                Gender
-              </label>
-              <h6 id="gender-value" name="gender-value" className="px-3">
-                {fileResult?.gender || "-"}
+                {item.isMask ? (
+                  <Tooltip title={item.value}>
+                    {item.value && item.value !== "---"
+                      ? getMastData(item.value)
+                      : "---"}
+                  </Tooltip>
+                ) : (
+                  item.value
+                )}
               </h6>
             </div>
-          </div>
-          <div className="col-2" id="gender" name="gender">
-            {SVGICON.faceToface}
-            <div className={styles.subItems}>
-              <label className="px-3" style={{ fontWeight: 600 }}>
-                Face to Face
-              </label>
-              <h6 id="gender-value" name="gender-value" className="px-3">
-                {/* {fileResult?.gender || "-"} */}
-                {fileDetails?.faceToFace === true
-                  ? "Yes"
-                  : fileDetails?.faceToFace === false
-                  ? "No"
-                  : "---"}
-              </h6>
-            </div>
-          </div>
-          <div className="col-4" id="gender" name="gender">
-            {SVGICON.visitType}
-            <div className={styles.subItems}>
-              <label className="px-3" style={{ fontWeight: 600 }}>
-                Visit Type
-              </label>
-              <h6 id="gender-value" name="gender-value" className="px-3">
-                {/* {fileResult?.gender || "-"} */}
-                {fileDetails?.visitType || "---"}
-              </h6>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </>
